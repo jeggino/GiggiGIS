bf14174a33eb1f4d63961958742192d5c05a4ec3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-05 12:31:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,50 +22,55 @@ def insert_period(date, sp, n, comment, geometry_type, geometry):
     """Returns the user on a successful user creation, otherwise raises and error"""
     return db_2.put({ "date":str(date), "sp": sp, "n_specimens":n, "comment": comment, "geometry_type":geometry_type, "geometry":geometry})
 
+with st.sidebar:
+    add_radio = st.radio(
+        "pagina",
+        ("pagina 1", "Pagina 2")
+    )
+
+if "pagina 1":
+    m = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
+    Draw().add_to(m)
+
+    c1, c2 = st.columns([3,2])
 
 
-m = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
-Draw().add_to(m)
+    with c1:
+        output = st_folium(m,width=500, height=700, returned_objects=["all_drawings"])
 
-c1, c2 = st.columns([3,2])
+    with c2:
 
+        try:
+            new_dict = output
+            new_dict["features"] = new_dict.pop("all_drawings")
 
-with c1:
-    output = st_folium(m,width=500, height=700, returned_objects=["all_drawings"])
+            if len(new_dict["features"]) > 1:
+                st.error("You cannot upload more than one survey at time!")
+                st.stop()
 
-with c2:
+            # error_empty = st.empty()
+            with st.form("entry_form", clear_on_submit=True):
+                date = st.date_input("Date")
+                sp = st.selectbox("Species", ["Anax imperator", "Ischnura elegans", "Lestes sponsa"])
+                n = st.number_input("Number of specimens:", min_value=0)
+                comment = st.text_input("", placeholder="Enter a comment here ...")
+                geometry_type = new_dict["features"][0]["geometry"]["type"]
+                geometry = new_dict["features"][0]   
 
-    try:
-        new_dict = output
-        new_dict["features"] = new_dict.pop("all_drawings")
-        
-        if len(new_dict["features"]) > 1:
-            st.error("You cannot upload more than one survey at time!")
-            st.stop()
+                submitted = st.form_submit_button("Save Data")
+                if submitted:
+                    insert_period(date, sp, n, comment, geometry_type, geometry)
+                    st.success('Data saved!', icon="✅")
 
-        # error_empty = st.empty()
-        with st.form("entry_form", clear_on_submit=True):
-            date = st.date_input("Date")
-            sp = st.selectbox("Species", ["Anax imperator", "Ischnura elegans", "Lestes sponsa"])
-            n = st.number_input("Number of specimens:", min_value=0)
-            comment = st.text_input("", placeholder="Enter a comment here ...")
-            geometry_type = new_dict["features"][0]["geometry"]["type"]
-            geometry = new_dict["features"][0]   
-
-            submitted = st.form_submit_button("Save Data")
-            if submitted:
-                insert_period(date, sp, n, comment, geometry_type, geometry)
-                st.success('Data saved!', icon="✅")
-
-    except:
-        st.info("mark an observation")
-        
-db_content = db_2.fetch().items
-df_point = pd.DataFrame(db_content)
-gpf = geopandas.GeoDataFrame.from_features(df_point["geometry"])
-map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
-folium.GeoJson(gpf.to_json()).add_to(map)
-st_folium(map)
-st.dataframe(df_point)
-st.warning("Qui é il casino!", icon="💀")
+        except:
+            st.info("mark an observation")
+if "pagina 2":
+    db_content = db_2.fetch().items
+    df_point = pd.DataFrame(db_content)
+    gpf = geopandas.GeoDataFrame.from_features(df_point["geometry"])
+    map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
+    folium.GeoJson(gpf.to_json()).add_to(map)
+    st_folium(map)
+    st.dataframe(df_point)
+    st.warning("Qui é il casino!", icon="💀")
 
