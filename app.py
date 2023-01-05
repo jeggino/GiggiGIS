8c1931c57a724f0b9c32cf9d8298ba2101b1458e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-05 09:54:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ deta = Deta(st.secrets["deta_key"])
 # Create a new database "example-db"
 db_2 = deta.Base("project_2")
 
+# -------------- FUNCTIONS --------------
+
+def insert_period(option_selectbox, options_multiselect, age, number, lat, lon, date):
+    """Returns the user on a successful user creation, otherwise raises and error"""
+    return db_2.put({"option_selectbox": option_selectbox, "options_multiselect": options_multiselect, "age": age, "number": number, "lat": lat, "lon": lon, "date":date})
+
 
 
 m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
@@ -29,39 +35,26 @@ with c2:
     try:
         new_dict = output
         new_dict["features"] = new_dict.pop("all_drawings")
-        new_dict["features"][0]["properties"] = {"option_selectbox":None,
-                                                 "options_multiselect":None,
-                                                 "age":None,
-                                                 "number":None,
-                                                 "date":None,}
-
-        error_empty = st.empty()
-        option_selectbox = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
-        new_dict["features"][0]["properties"]["option_selectbox"] = option_selectbox
-
-        options_multiselect = st.multiselect( 'What are your favorite colors',['Green', 'Yellow', 'Red', 'Blue'],['Yellow', 'Red'])
-        new_dict["features"][0]["properties"]["options_multiselect"] = options_multiselect
-
-        age = st.slider('How old are you?', 0, 130, 25)
-        new_dict["features"][0]["properties"]["age"] = age
-
-        number = st.number_input('Insert a number')
-        new_dict["features"][0]["properties"]["number"] = number
-
-        date = st.date_input("When\'s your birthday",datetime.date(2019, 7, 6))
-        new_dict["features"][0]["properties"]["date"] = date
-
+        
         if len(new_dict["features"]) > 1:
             st.error("You cannot upload more than one survey at time!")
             st.stop()
-            
-        st.write(new_dict)
+      
+        # error_empty = st.empty()
+        option_selectbox = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
+        options_multiselect = st.multiselect( 'What are your favorite colors',['Green', 'Yellow', 'Red', 'Blue'],['Yellow', 'Red'])
+        age = st.slider('How old are you?', 0, 130, 25)
+        number = st.number_input('Insert a number')
+        date = st.date_input("When\'s your birthday",datetime.date(2019, 7, 6))
+        lat = new_dict["features"][0]["geometry"]["coordinates"][0]
+        lon = new_dict["features"][0]["geometry"]["coordinates"][1]
 
+        
         with st.form("entry_form_2", clear_on_submit=False):
             submitted = st.form_submit_button("Save Data")
             if submitted:
-                st.warning("Ti stanno prendendo per il culo!", icon="💀")
-                db_2.put({"info":new_dict})
+                insert_period(option_selectbox, options_multiselect, age, number, lat, lon, date)
+                # st.warning("Ti stanno prendendo per il culo!", icon="💀")
                 st.success('Data saved!', icon="✅")
 
 
