5be8bd8dffac2372bf12a19c5fc11293e50d7148
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-05 13:21:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if add_radio == "📝":
 elif add_radio == "🗺️":
     db_content = db_2.fetch().items
     df_point = pd.DataFrame(db_content)
-    gpf = geopandas.GeoDataFrame(db_content)
+    gpf = geopandas.GeoDataFrame(df_point)
     
     gpf = gpf.from_features(df_point["geometry"])
     st.write(gpf.to_json())
