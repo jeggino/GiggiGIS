70b6e5ca262c1b884f1b69dc80cca3d80cf3e5da
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-11 11:45:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -114,10 +114,10 @@ elif add_radio == "🗺️":
                                                         style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
                                                       )
                           ).add_to(map_2)
-        output_2 = st_folium(map_2, width=500, height=700, returned_objects=["all_drawings"]["properties"]["image_name"])
+        output_2 = st_folium(map_2, width=500, height=700, returned_objects=["all_drawings"])
         
     with c2:
-        output_2
+        output_2["properties"]["image_name"]
         
    
 
