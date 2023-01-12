4580e1a17aa267757587ca6b7f132432a394b643
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-12 14:11:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ elif add_radio == "🗺️":
 #                                                           )
                           ).add_to(map)
 
-            output = st_folium(map, width=1000)
+            output = st_folium(map, width=1200)
                 
             st.sidebar.write(output)
 
