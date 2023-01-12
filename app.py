4907d3394b69256dc4ffe638760243ebd8cc8d1a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-12 08:57:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -155,10 +155,8 @@ elif add_radio == "🗺️":
             
         with c2:
             uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-            for uploaded_file in uploaded_files:
-                bytes_data = uploaded_file.read()
-                st.write("filename:", uploaded_file.name)
-                st.write(bytes_data)
+            df = pd.read_csv(uploaded_files)
+            st.dataset(df)
         
    
 
