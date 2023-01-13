df41ba7412ebf9e9b86d52f45bd30cc43d44da56
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-13 08:13:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,17 +49,18 @@ def insert_json(json):
     """Returns the user on a successful user creation, otherwise raises and error"""
     return db_3.put({"json":json})
 
-with st.sidebar:
-    add_radio = st.radio("", horizontal=False, options = ["📝", "🗺️"], label_visibility="collapsed")
+# with st.sidebar:
+#     add_radio = st.radio("", horizontal=False, options = ["📝", "🗺️"], label_visibility="collapsed")
 
-if add_radio == "📝":      
+# if add_radio == "📝":      
                 
-    m = folium.Map(location=[lat, lon], zoom_start=18)
-    Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
-    Fullscreen().add_to(m)
-    output = st_folium(m, width=500, height=700, returned_objects=["all_drawings"])
+m = folium.Map(location=[lat, lon], zoom_start=18)
+Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
+Fullscreen().add_to(m)
+output = st_folium(m, width=1200, height=500, returned_objects=["all_drawings"])
         
-                   
+with st.form("entry_form", clear_on_submit=True):
+    
     with st.sidebar:
 
         try:
@@ -70,7 +71,7 @@ if add_radio == "📝":
                 st.error("You cannot upload more than one survey at time!")
                 st.stop()
 
-            with st.form("entry_form", clear_on_submit=True):
+#             with st.form("entry_form", clear_on_submit=True):
                 date = st.date_input("Date")
                 sp = st.selectbox("Species", ["Anax imperator", "Ischnura elegans", "Lestes sponsa"])
                 n = st.number_input("Number of specimens:", min_value=0)
@@ -107,74 +108,74 @@ if add_radio == "📝":
         except:
             st.info("mark an observation")
             
-elif add_radio == "🗺️":
+# elif add_radio == "🗺️":
     
-    with st.sidebar:
-        option = st.selectbox('',('Cloud', 'Load dataset'))
+#     with st.sidebar:
+#         option = st.selectbox('',('Cloud', 'Load dataset'))
 
     
-    if option == 'Cloud':
+#     if option == 'Cloud':
         
-        c1, c2 = st.columns([3,2])
+#         c1, c2 = st.columns([3,2])
     
-        with c1:
-            db_content = db_3.fetch().items
-            df_point = pd.DataFrame(db_content)
+#         with c1:
+#             db_content = db_3.fetch().items
+#             df_point = pd.DataFrame(db_content)
 
-            map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
-            for i in df_point["json"].to_list():
-                folium.GeoJson(i,
-                              tooltip=folium.GeoJsonTooltip(fields= ["date", "sp", "n", "comment"],
-                                                            aliases=["Date: ", "Species: ", "Nember of specimens: ", "Comment: "],
-                                                            labels=True,
-                                                            style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
-                                                          )
-                              ).add_to(map)
-            output_2 = st_folium(map, width=500, height=700)
-
-        with c2:
-            try:
-                name = output_2["last_active_drawing"]["properties"]["image_name"]
-                comment = output_2["last_active_drawing"]["properties"]["comment"]
-                with st.container():
-                    image = drive.get(name).read()
-                    st.image(image, caption=comment, use_column_width='always')
-            except:
-                st.info("No image")
-
-                
-    elif option == 'Load dataset':
-                
-        try:
-            
-            with st.sidebar:
-                uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
-                df = pd.read_csv(uploaded_file)
-                city = st.multiselect("", df["stad"].unique(), default=None,label_visibility="collapsed")
-                nlnaam = st.multiselect("", df["nlnaam"].unique(), default=None,label_visibility="collapsed")
-                functie = st.multiselect("", df["functie"].unique(), default=None,label_visibility="collapsed")
-                
-                
-                
-                
-            filter = df[(df["stad"].isin(city)) & (df["nlnaam"].isin(nlnaam)) & (df["functie"].isin(functie))]
-            gdf = gpd.GeoDataFrame(filter, geometry=gpd.points_from_xy(filter.lon,filter.lat))
-
-            map = folium.Map(location=[filter.lat.mean(),filter.lon.mean()], zoom_start=8)
-            folium.GeoJson(gdf.to_json(),
-#                               tooltip=folium.GeoJsonTooltip(fields= ["Data", "Profondità", "magnitudo_score"],
-#                                                             aliases=["Date: ", "Deepness: ", "Magnitudo: "],
+#             map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
+#             for i in df_point["json"].to_list():
+#                 folium.GeoJson(i,
+#                               tooltip=folium.GeoJsonTooltip(fields= ["date", "sp", "n", "comment"],
+#                                                             aliases=["Date: ", "Species: ", "Nember of specimens: ", "Comment: "],
 #                                                             labels=True,
 #                                                             style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
 #                                                           )
-                          ).add_to(map)
+#                               ).add_to(map)
+#             output_2 = st_folium(map, width=500, height=700)
 
-            output = st_folium(map, width=1200)
+#         with c2:
+#             try:
+#                 name = output_2["last_active_drawing"]["properties"]["image_name"]
+#                 comment = output_2["last_active_drawing"]["properties"]["comment"]
+#                 with st.container():
+#                     image = drive.get(name).read()
+#                     st.image(image, caption=comment, use_column_width='always')
+#             except:
+#                 st.info("No image")
+
                 
-            st.sidebar.write(output)
+#     elif option == 'Load dataset':
+                
+#         try:
+            
+#             with st.sidebar:
+#                 uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
+#                 df = pd.read_csv(uploaded_file)
+#                 city = st.multiselect("", df["stad"].unique(), default=None,label_visibility="collapsed")
+#                 nlnaam = st.multiselect("", df["nlnaam"].unique(), default=None,label_visibility="collapsed")
+#                 functie = st.multiselect("", df["functie"].unique(), default=None,label_visibility="collapsed")
+                
+                
+                
+                
+#             filter = df[(df["stad"].isin(city)) & (df["nlnaam"].isin(nlnaam)) & (df["functie"].isin(functie))]
+#             gdf = gpd.GeoDataFrame(filter, geometry=gpd.points_from_xy(filter.lon,filter.lat))
 
-        except:
-            st.stop()
+#             map = folium.Map(location=[filter.lat.mean(),filter.lon.mean()], zoom_start=8)
+#             folium.GeoJson(gdf.to_json(),
+# #                               tooltip=folium.GeoJsonTooltip(fields= ["Data", "Profondità", "magnitudo_score"],
+# #                                                             aliases=["Date: ", "Deepness: ", "Magnitudo: "],
+# #                                                             labels=True,
+# #                                                             style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
+# #                                                           )
+#                           ).add_to(map)
+
+#             output = st_folium(map, width=1200)
+                
+#             st.sidebar.write(output)
+
+#         except:
+#             st.stop()
 
             
         
