1adf08307b09516bb7295f15dcffef30fcb373af
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-14 08:13:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ import datetime
 
 from deta import Deta
 
+# This code is different for each deployed app.
+CURRENT_THEME = "blue"
+IS_DARK_THEME = True
+EXPANDER_TEXT = """
+    This is a custom theme. You can enable it by copying the following code
+    to `.streamlit/config.toml`:
+    ```python
+    [theme]
+    primaryColor = "#E694FF"
+    backgroundColor = "#00172B"
+    secondaryBackgroundColor = "#0083B8"
+    textColor = "#C6CDD4"
+    font = "sans-serif"
+    ```
+    """
 
 st.set_page_config(
     page_title="GiggiGIS",
