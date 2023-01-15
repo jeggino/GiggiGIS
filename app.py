ec5cc0e352552227c47974a626cda7da298a95eb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-15 06:27:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,13 +33,13 @@ def insert_json(json):
     return db_3.put({"json":json})
 
                 
-m = folium.Map(location=[44.266308, 11.719301], zoom_start=3)
+m = folium.Map(location=[44.266308, 11.719301], zoom_start=3, width=300, height=500,)
 Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
 Fullscreen().add_to(m)
 LocateControl(auto_start=False).add_to(m)
 
 with st.container():
-    output = st_folium(m, width=300, height=500, returned_objects=["all_drawings"])
+    output = st_folium(m,  returned_objects=["all_drawings"])
             
 with st.sidebar:
 
