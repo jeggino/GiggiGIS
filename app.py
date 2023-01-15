5d0acc5c48dd9a2a6cbf044a43f194b3c2a46262
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-15 06:11:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 
 from folium.plugins import Draw, Fullscreen, LocateControl
 from streamlit_folium import st_folium
-# from streamlit_js_eval import get_geolocation
 
 import pandas as pd
 import geopandas as gpd
@@ -26,22 +25,6 @@ deta = Deta(st.secrets["deta_key"])
 db_3 = deta.Base("GiggiGIS_data")
 drive = deta.Drive("GiggiGIS_pictures")
 
-# loc = get_geolocation()
-# lat = loc['coords']['latitude']
-# lon = loc['coords']['longitude']
-
-# # ---CUSTUMIZE---
-
-# st.markdown("""
-#     <style>
-#       section[data-testid="stSidebar"][aria-expanded="true"]{
-#         width: 100% !important;
-#       }
-#       section[data-testid="stSidebar"][aria-expanded="false"]{
-#         width: 100% !important;
-#       }
-#     </style>""", unsafe_allow_html=True)
-
 # -------------- FUNCTIONS --------------
 
 
@@ -49,10 +32,6 @@ def insert_json(json):
     """Returns the user on a successful user creation, otherwise raises and error"""
     return db_3.put({"json":json})
 
-# with st.sidebar:
-#     add_radio = st.radio("", horizontal=False, options = ["📝", "🗺️"], label_visibility="collapsed")
-
-# if add_radio == "📝":      
                 
 m = folium.Map(location=[44.266308, 11.719301], zoom_start=3)
 Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
@@ -62,9 +41,7 @@ LocateControl(auto_start=False).add_to(m)
 
 with st.container():
     output = st_folium(m, width=1200, height=500, returned_objects=["all_drawings"])
-        
-# with st.form("entry_form", clear_on_submit=True):
-    
+            
 with st.sidebar:
 
     try:
@@ -110,82 +87,7 @@ with st.sidebar:
         st.stop()
 
     
-                
-
-
-
-    
-            
-# elif add_radio == "🗺️":
-    
-#     with st.sidebar:
-#         option = st.selectbox('',('Cloud', 'Load dataset'))
-
-    
-#     if option == 'Cloud':
-        
-#         c1, c2 = st.columns([3,2])
-    
-#         with c1:
-#             db_content = db_3.fetch().items
-#             df_point = pd.DataFrame(db_content)
-
-#             map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
-#             for i in df_point["json"].to_list():
-#                 folium.GeoJson(i,
-#                               tooltip=folium.GeoJsonTooltip(fields= ["date", "sp", "n", "comment"],
-#                                                             aliases=["Date: ", "Species: ", "Nember of specimens: ", "Comment: "],
-#                                                             labels=True,
-#                                                             style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
-#                                                           )
-#                               ).add_to(map)
-#             output_2 = st_folium(map, width=500, height=700)
-
-#         with c2:
-#             try:
-#                 name = output_2["last_active_drawing"]["properties"]["image_name"]
-#                 comment = output_2["last_active_drawing"]["properties"]["comment"]
-#                 with st.container():
-#                     image = drive.get(name).read()
-#                     st.image(image, caption=comment, use_column_width='always')
-#             except:
-#                 st.info("No image")
-
-                
-#     elif option == 'Load dataset':
-                
-#         try:
-            
-#             with st.sidebar:
-#                 uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
-#                 df = pd.read_csv(uploaded_file)
-#                 city = st.multiselect("", df["stad"].unique(), default=None,label_visibility="collapsed")
-#                 nlnaam = st.multiselect("", df["nlnaam"].unique(), default=None,label_visibility="collapsed")
-#                 functie = st.multiselect("", df["functie"].unique(), default=None,label_visibility="collapsed")
-                
-                
-                
-                
-#             filter = df[(df["stad"].isin(city)) & (df["nlnaam"].isin(nlnaam)) & (df["functie"].isin(functie))]
-#             gdf = gpd.GeoDataFrame(filter, geometry=gpd.points_from_xy(filter.lon,filter.lat))
-
-#             map = folium.Map(location=[filter.lat.mean(),filter.lon.mean()], zoom_start=8)
-#             folium.GeoJson(gdf.to_json(),
-# #                               tooltip=folium.GeoJsonTooltip(fields= ["Data", "Profondità", "magnitudo_score"],
-# #                                                             aliases=["Date: ", "Deepness: ", "Magnitudo: "],
-# #                                                             labels=True,
-# #                                                             style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
-# #                                                           )
-#                           ).add_to(map)
-
-#             output = st_folium(map, width=1200)
-                
-#             st.sidebar.write(output)
-
-#         except:
-#             st.stop()
-
-            
+              
         
         
    
