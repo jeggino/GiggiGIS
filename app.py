1393a184d53547606ddaeb5e49f470efa1444c64
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-17 08:09:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -107,131 +107,131 @@ def password_generator(length):
 
 # --- APP ---
 
-
-
-
 def main():
-    with st.sidebar:
-        option = st.radio("", options=('📝 Data Entry', '🗺️ Data Visualization'), horizontal=True, label_visibility="visible")
     
-    "---"
-    
-    if option == '📝 Data Entry':
+    while True:
+        
+        with st.sidebar:
+            option = st.radio("", options=('📝 Data Entry', '🗺️ Data Visualization'), horizontal=True, label_visibility="visible")
 
-            m = folium.Map(location=[44.266308, 11.719301], zoom_start=3, width=150, height=250)
-            Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
-            Fullscreen().add_to(m)
-            LocateControl(auto_start=True).add_to(m)
+        "---"
 
-            with st.container():
-                output = st_folium(m,  returned_objects=["all_drawings"])
+        if option == '📝 Data Entry':
 
-            if output:
+                m = folium.Map(location=[44.266308, 11.719301], zoom_start=3, width=150, height=250)
+                Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
+                Fullscreen().add_to(m)
+                LocateControl(auto_start=True).add_to(m)
 
-                with st.sidebar:
+                with st.container():
+                    output = st_folium(m,  returned_objects=["all_drawings"])
 
-                    date = st.date_input("Date")
-                    sp = st.selectbox("Soort", BAT_NAMES)
-                    n = st.number_input("Number of specimens:", min_value=0)
-                    comment = st.text_input("", placeholder="Enter a comment here ...")
-                    with st.expander("Upload a picture"):
-                        uploaded_file = st.camera_input("")
-
-                    try:
-
-                        new_dict = output
-                        new_dict["features"] = new_dict.pop("all_drawings")
-                        key = password_generator(12)
-
-                        if len(new_dict["features"]) > 1:
-                            st.error("You cannot upload more than one survey at time!")
-                            st.stop()
-
-                        else:
-
-
-                            new_dict["features"][0]["properties"]["date"] = str(date)
-                            new_dict["features"][0]["properties"]["sp"] = sp
-                            new_dict["features"][0]["properties"]["n"] = n
-                            new_dict["features"][0]["properties"]["comment"] = comment
-                            new_dict["features"][0]["properties"]["id"] = key
-
-                            with st.form("entry_form", clear_on_submit=True):
-                                submitted = st.form_submit_button("Save Data")
-                                if submitted:
-                                    if uploaded_file is not None:
-                                        bytes_data = uploaded_file.getvalue()
-                                        drive.put(f"{key}.jpeg", data=bytes_data)            
-                                        new_dict["features"][0]["properties"]["image_name"] = f"{key}.jpeg"
-                                        insert_json(new_dict,key)
-                                    else:
-                                        new_dict["features"][0]["properties"]["image_name"] = None
-                                        insert_json(new_dict,key)
-
-                                    st.success('Data saved!', icon="✅")
-
-
-                    except:
-
-                        st.info("Mark an observation")
-                        st.stop()
-
-
-
-    elif option == "🗺️ Data Visualization":
-
-        try:
-            db_content = db.fetch().items
-            df_point = pd.DataFrame(db_content)
-
-            map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
-            LocateControl(auto_start=True).add_to(map)
-
-            for i in df_point["json"].to_list():
-                folium.GeoJson(i,
-                           tooltip=folium.GeoJsonTooltip(fields= ["date", "sp", "n", "comment"],
-                                                         aliases=["Date: ", "Species: ", "Nember of specimens: ", "Comment: "],
-                                                         labels=True,
-                                                         style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
-                                                       )
-                           ).add_to(map)
-
-            output = st_folium(map, width=500, height=700, returned_objects=["last_active_drawing"])
-
-            with st.sidebar:
-
-                try:
-                    id = output["last_active_drawing"]["properties"]["id"]
-                    name = output["last_active_drawing"]["properties"]["image_name"]
+                if output:
 
                     with st.sidebar:
 
+                        date = st.date_input("Date")
+                        sp = st.selectbox("Soort", BAT_NAMES)
+                        n = st.number_input("Number of specimens:", min_value=0)
+                        comment = st.text_input("", placeholder="Enter a comment here ...")
+                        with st.expander("Upload a picture"):
+                            uploaded_file = st.camera_input("")
+
                         try:
 
-                            res = drive.get(name).read()
-                            with st.expander("See image"):
-                                st.image(res)
+                            new_dict = output
+                            new_dict["features"] = new_dict.pop("all_drawings")
+                            key = password_generator(12)
 
-                            with st.form("entry_form", clear_on_submit=True):
-                                submitted = st.form_submit_button("Deleted Data")
-                                if submitted:
-                                    db.delete(id)
-                                    drive.delete(name)
-                                    st.success('Data deleted!', icon="✅")
+                            if len(new_dict["features"]) > 1:
+                                st.error("You cannot upload more than one survey at time!")
+                                st.stop()
+
+                            else:
+
+
+                                new_dict["features"][0]["properties"]["date"] = str(date)
+                                new_dict["features"][0]["properties"]["sp"] = sp
+                                new_dict["features"][0]["properties"]["n"] = n
+                                new_dict["features"][0]["properties"]["comment"] = comment
+                                new_dict["features"][0]["properties"]["id"] = key
+
+                                with st.form("entry_form", clear_on_submit=True):
+                                    submitted = st.form_submit_button("Save Data")
+                                    if submitted:
+                                        if uploaded_file is not None:
+                                            bytes_data = uploaded_file.getvalue()
+                                            drive.put(f"{key}.jpeg", data=bytes_data)            
+                                            new_dict["features"][0]["properties"]["image_name"] = f"{key}.jpeg"
+                                            insert_json(new_dict,key)
+                                        else:
+                                            new_dict["features"][0]["properties"]["image_name"] = None
+                                            insert_json(new_dict,key)
+
+                                        st.success('Data saved!', icon="✅")
+
 
                         except:
-                            st.warning('No picture saved!', icon="⚠️")
-                            with st.form("entry_form", clear_on_submit=True):
-                                submitted = st.form_submit_button("Deleted Data")
-                                if submitted:
-                                    db.delete(id)
-                                    st.success('Data deleted!', icon="✅")
+
+                            st.info("Mark an observation")
+                            st.stop()
 
 
-                except:
-                    st.info("Select an observation")
 
-        except:
-            st.error('No data yet!', icon="🚨")
+        elif option == "🗺️ Data Visualization":
+
+            try:
+                db_content = db.fetch().items
+                df_point = pd.DataFrame(db_content)
+
+                map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
+                LocateControl(auto_start=True).add_to(map)
+
+                for i in df_point["json"].to_list():
+                    folium.GeoJson(i,
+                               tooltip=folium.GeoJsonTooltip(fields= ["date", "sp", "n", "comment"],
+                                                             aliases=["Date: ", "Species: ", "Nember of specimens: ", "Comment: "],
+                                                             labels=True,
+                                                             style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 20px;")
+                                                           )
+                               ).add_to(map)
+
+                output = st_folium(map, width=500, height=700, returned_objects=["last_active_drawing"])
+
+                with st.sidebar:
+
+                    try:
+                        id = output["last_active_drawing"]["properties"]["id"]
+                        name = output["last_active_drawing"]["properties"]["image_name"]
+
+                        with st.sidebar:
+
+                            try:
+
+                                res = drive.get(name).read()
+                                with st.expander("See image"):
+                                    st.image(res)
+
+                                with st.form("entry_form", clear_on_submit=True):
+                                    submitted = st.form_submit_button("Deleted Data")
+                                    if submitted:
+                                        db.delete(id)
+                                        drive.delete(name)
+                                        st.success('Data deleted!', icon="✅")
+
+                            except:
+                                st.warning('No picture saved!', icon="⚠️")
+                                with st.form("entry_form", clear_on_submit=True):
+                                    submitted = st.form_submit_button("Deleted Data")
+                                    if submitted:
+                                        db.delete(id)
+                                        st.success('Data deleted!', icon="✅")
+
+
+                    except:
+                        st.info("Select an observation")
+
+            except:
+                st.error('No data yet!', icon="🚨")
             
 main()
