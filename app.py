a83a3fefe8e575e67a8ed5481cdaae7264ac7953
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-18 16:17:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -308,12 +308,12 @@ elif option == "bla bla":
     
     while True:
         
-        
+        m = folium.Map(location=[44.266308, 11.719301], zoom_start=3, width=150, height=250)
+        Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
+        Fullscreen().add_to(m)
+        LocateControl(auto_start=True).add_to(m)
         with st.form("my_form"):
-            m = folium.Map(location=[44.266308, 11.719301], zoom_start=3, width=150, height=250)
-            Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
-            Fullscreen().add_to(m)
-            LocateControl(auto_start=True).add_to(m)
+            
 
             output = st_folium(m,  returned_objects=["all_drawings"])
             st.write("Inside the form")
