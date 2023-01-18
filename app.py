5413d556db9bfa8fab539dd7eeb3225995e301e8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-18 07:15:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ elif option == "bla bla":
     import time
 
     with st.empty():
-        for seconds in range(3):
+        for seconds in range(10):
             st.write(f"⏳ {seconds} seconds have passed")
             time.sleep(1)
         st.write("✔️ 1 minute over!")
