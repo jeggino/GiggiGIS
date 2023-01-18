c66ff445bec8aa5828d0eb89eddd19eeff7e21ef
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-18 17:27:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -273,37 +273,28 @@ class NewStudent:
     def __init__(self, page_id):
         st.title(f"Student N°{page_id}")
         self.name = st.text_input("Name")
-#         self.age = st.text_input("Age")
         self.json = map()
 
 
 def main():
     placeholder = st.empty()
-    placeholder2 = st.empty()
 
     while True:    
         num = st.session_state.num
+     
+        with placeholder.form(key=str(num)):
+            new_student = NewStudent(page_id=num)     
+            st.warning('This is a warning', icon="⚠️")
 
-        if placeholder2.button('end', key=num):
-            placeholder2.empty()
-            df = pd.DataFrame(st.session_state.data)
-            st.dataframe(df)
-            break
-        else:        
-            with placeholder.form(key=str(num)):
-                new_student = NewStudent(page_id=num)     
-                st.warning('This is a warning', icon="⚠️")
-
-                if st.form_submit_button('register'):                
-                    st.session_state.data.append({
-                        'id': num, 'name': new_student.name, "output": new_student.json})
-                    st.session_state.num += 1
-                    
-                    placeholder.empty()
-                    
-                    placeholder2.empty()
-                else:
-                    st.stop()
+            if st.form_submit_button('register'):                
+                st.session_state.data.append({
+                    'id': num, 'name': new_student.name, "output": new_student.json})
+                st.session_state.num += 1                
+                placeholder.empty()                  
+                df = pd.DataFrame(st.session_state.data)
+                st.dataframe(df)
+            else:
+                st.stop()
 
 main()
 
