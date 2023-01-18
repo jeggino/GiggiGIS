63f2a87c1b39bee2b5efc08b0d2b8afd4fa6101d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-18 18:15:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -253,20 +253,23 @@ def password_generator(length):
 import streamlit as st
 import pandas as pd
 
-
+def map():
+    m = folium.Map(location=[44.266308, 11.719301], zoom_start=3, width=150, height=250)
+    Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
+    Fullscreen().add_to(m)
+    LocateControl(auto_start=True).add_to(m)
+    return m
+    
 def main():
     while True:
 
         if st.button('Say hello'):
             st.write("ok")
-            st.experimental_rerun()
+            
 
         else:
-            m = folium.Map(location=[44.266308, 11.719301], zoom_start=3, width=150, height=250)
-            Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
-            Fullscreen().add_to(m)
-            LocateControl(auto_start=True).add_to(m)
-            output = st_folium(m,  returned_objects=["all_drawings"])
+            
+            output = st_folium(map(),  returned_objects=["all_drawings"])
             st.write(output)
             st.stop()
             
