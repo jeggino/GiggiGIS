121040e1c50418ab40c5313c2d2cbd5bb0db3f21
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-20 08:12:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from deta import Deta
 import string
 import random
 
+from streamlit_elements import elements, mui, html
 
 
 st.set_page_config(
@@ -235,6 +236,17 @@ if selected == '📝':
             uploaded_file = st.camera_input("")
             
     input_data(date,sp,gedrag,functie,verblijf,aantal,opmerking,uploaded_file)
+    
+    with elements("media_player"):
+
+        # Play video from many third-party sources: YouTube, Facebook, Twitch,
+        # SoundCloud, Streamable, Vimeo, Wistia, Mixcloud, DailyMotion and Kaltura.
+        #
+        # This element is powered by ReactPlayer (GitHub link below).
+
+        from streamlit_elements import media
+
+        media.Player(url="https://www.youtube.com/watch?v=iik25wqIuFo", controls=True)
             
     
     
