02f241f51d6cf2c4f23fe9949b1018a96a8573de
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 07:55:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ elif selected == "Data visualization":
             "---"
             
             start_date, end_date = st.date_input('start date  - end date :', [])
-            if start_date < end_date:
+            if start_date <= end_date:
                 pass
             else:
                 st.error('Error: End date must fall after start date.')
