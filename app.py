2f64919c354a05343745972db78047e3455585c7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 09:16:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ elif selected == "Data visualization":
         
         df_point = pd.DataFrame(db_content)
         
-        df_point['date'] = df_point['date'].astype('datetime64[ns]')
+        df_point['date'] = pd.to_datetime(df_point['date'], format='%y%m%d')
         st.write(df_point['date'][0])
         st.write(start_date)
         st.write(end_date)
