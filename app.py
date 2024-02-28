ed981444729069ad0018d1cf2eeb2a185bc937d5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-02-28 14:35:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -230,44 +230,8 @@ elif selected == "Data visualization":
         
         db_content = load_dataset()
         df_point = pd.DataFrame(db_content)
-        
-        with st.sidebar:
             
-#             soortgroup = st.selectbox("Soortgroep", ['Vleermuizen',  'Vogels'])
-#             start_date, end_date = st.date_input('start date  - end date :', [date.today(),date.today()])
-#             geometry_type = st.multiselect("Type geometrie", ['Point',  'LineString', 'Polygon'])
-# #             st.warning("HERE IS THE PROBLEM!!!!!", icon="💀")
 
-            
-                
-#             if start_date <= end_date:
-#                 pass
-#             else:
-#                 st.error('Error: End date must fall after start date.')    
-            
-            # if soortgroup == 'Vleermuizen':
-            
-            #     sp = st.multiselect("Soort", BAT_NAMES)
-            #     with st.expander("Kies het gedrag, de functie en het verblijf", expanded=False):
-            #         gedrag = st.multiselect("Gedrag", BAT_BEHAVIOURS, BAT_BEHAVIOURS) 
-            #         functie = st.multiselect("Functie", BAT_FUNCTIE, BAT_FUNCTIE) 
-            #         verblijf = st.multiselect("Verblijf", BAT_VERBLIJF, BAT_VERBLIJF) 
-            
-            # elif soortgroup == 'Vogels':
-
-            #     sp = st.multiselect("Soort", BIRD_NAMES)
-            #     with st.expander("Kies het gedrag, de functie en het verblijf", expanded=False):
-            #         gedrag = st.multiselect("Gedrag", BIRD_BEHAVIOURS,BIRD_BEHAVIOURS) 
-            #         functie = st.multiselect("Functie", BIRD_FUNCTIE,BIRD_FUNCTIE) 
-            #         verblijf = st.multiselect("Verblijf", BIRD_VERBLIJF,BIRD_VERBLIJF) 
-        
-        # df_point = pd.DataFrame(db_content)        
-        # df_point['date'] = pd.to_datetime(df_point['date']).dt.date
-        
-
-        # mask_date = (df_point['date'] >= start_date) & (df_point['date'] <= end_date)
-        # mask_geometry_type = (df_point.geometry_type.isin(geometry_type))
-        # df_filter = df_point[mask_geometry_type & mask_date & (df_point.sp.isin(sp)) & (df_point.gedrag.isin(gedrag)) & (df_point.functie.isin(functie)) & (df_point.verblijf.isin(verblijf))]
 
         df_filter = df_point
         map = folium.Map(location=[52.370898, 4.898065], zoom_start=8)
