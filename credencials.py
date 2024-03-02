a9dc1162915fbb650d3c8cbd68cda5a3173b3507
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-02 17:03:48 +0100

    Create credencials.py

---
+++ credencials.py
@@ -0,0 +1,6 @@
+dict = {
+  'daan@elskenecologie.nl': {'name': 'Daan',
+              'password':'elskenecologie_2024'},
+  'jeggino@gmail.com' : {'name': 'Luigi',
+             'password': 'elskenecologie_2024'}
+}
