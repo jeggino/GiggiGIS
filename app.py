c1a3262e312cbffcff614198442bebf38ce34074
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-03 02:31:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,62 +39,6 @@ footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
 
-# # --- VARIABLES---
-
-# PASSWORD = "GiggiGIS"
-
-# GROUP = ["🪶 Vogels", "🦇 Vleermuizen", "🏠 Vleermuiskast"]
-
-# GROUP_DICT = {"🪶 Vogels":"Vogels", "🦇 Vleermuizen":"Vleermuizen", "🏠 Vleermuiskast":"Vleermuiskast"}
-
-# BAT_NAMES = ['Laatvlieger', 'Gewone dwergvleermuis', 'Watervleermuis',
-#        'Rosse vleermuis', 'Ruige dwergvleermuis', 'Meervleermuis',
-#        'Bosvleermuis', 'Franjestaart', 'Vleermuis onbekend',
-#        'Myotis spec.', 'Vale vleermuis', 'Gewone grootoorvleermuis',
-#        'Ingekorven vleermuis', 'Baardvleermuis', 'Brandts vleermuis',
-#        'Kleine dwergvleermuis', 'Grijze grootoorvleermuis',
-#        'Bechsteins vleermuis', 'Tweekleurige vleermuis',
-#        'Dwergvleermuis spec.', 'Plecotus spec.', 'Mopsvleermuis']
-
-# BIRD_NAMES = ['Gierzwaluw','Huismus']
-
-# BAT_BEHAVIOURS = ['foeragerend', 'roepend','verplaatsend (vliegroute)', 'sociale roep', 'uitvliegend','invliegend', 'overvliegend', 
-#            'nest-indicerend gedrag', 'zwermend', 'sporen', 'balts', 'verkeersslachtoffer','bezet nest']
-
-# BIRD_BEHAVIOURS = ['bezet nest', 'nest-indicerend gedrag', 'overvliegend',
-#        'foeragerend', 'ter plaatse', 'roepend', 'baltsend / zingend',
-#        'nestbouw', 'invliegend', 'uitvliegend', 'jagend',
-#        'roepend vanuit gebouw', 'baltsend / zingend op gebouw',
-#        'baltsend / zingend in vegetatie, struik of boom', 'sporen',
-#        'geen / onbekend', 'onbekend', 'vondst', 'paaiend',
-#        'verplaatsend (vliegroute)', 'copula']
-
-# BAT_FUNCTIE = ['geen / onbekend', 'zomerverblijfplaats in gebouw', 'paarverblijfplaats in gebouw','vliegroute', 'kraamverblijfplaats in gebouw',
-#            'vliegroute (bomen)', 'vliegroute (water)', 'zomerverblijfplaats in boom', 'paarverblijfplaats in boom', 
-#            'kraamverblijfplaats in boom', 'winterverblijfplaats in gebouw', 'massa winterverblijfplaats', 
-#            'essentieel foerageergebied (water)', 'winterverblijfplaats in bloei', 'essentieel foerageergebied (bomen)', 'vastgesteld territorium',
-#            'essentieel foerageergebied (grasland)']
-
-# BIRD_FUNCTIE = ['nestlocatie', 'geen / onbekend', 'vastgesteld territorium',
-#        'functioneel leefgebied', 'mogelijke nestlocatie',
-#        'voortplantingsbiotoop', 'winterverblijfplaats in boom',
-#        'zomerverblijfplaats']
-
-# BAT_VERBLIJF = ['geen / onbekend', 'dakgoot', 'spouwmuur', 'daklijst',
-#        'kantpan', 'regenpijp', 'holte', 'raamkozijn', 'luik', 'scheur',
-#        'schoorsteen', 'gevelbetimmering', 'nokpan', 'dakpan',
-#        'vleermuiskast', 'openingen in dak', 'dakkapel', 'schors']
-
-# VLEERMUISKAST_VERBLIJF = ["Op boom", "Op gebouw"]
-
-# BIRD_VERBLIJF = ['dakgoot', 'kantpan', 'zonnepaneel', 'nokpan', 'nestkast',
-#        'gevelbetimmering', 'openingen in dak', 'regenpijp',
-#        'geen / onbekend', 'dakpan', 'spouwmuur', 'onder dakrand',
-#        'raamkozijn', 'luik', 'schoorsteen', 'daklijst', 'dakkapel',
-#        'in struweel / struiken', 'holte', 'op / bij nest in boom',
-#        'scheur', 'vleermuiskast']
-
-
 
 # --- CONNECT TO DETA ---
 deta = Deta(st.secrets["deta_key"])
