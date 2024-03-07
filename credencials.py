9842b273be192913599c5c62e42f62958d284311
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-07 10:05:07 +0100

    Update credencials.py

--- credencials.py
+++ credencials.py
@@ -1,11 +1,3 @@
-dict = {
-  'daan@elskenecologie.nl': {'name': 'Daan',
-              'password':'elskenecologie_2024'},
-  'jeggino@gmail.com': {'name': 'Luigi Giugliano',
-             'password': 'elskenecologie_2024'}
-}
-
-
 # --- VARIABLES---
 
 PASSWORD = "GiggiGIS"
