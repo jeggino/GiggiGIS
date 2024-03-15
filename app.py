f6f79e1475dc88f53ae9e47b6b3b3961f9b56d64
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-15 09:28:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,14 +49,11 @@ elif st.session_state["authentication_status"]:
     waarnemer = st.session_state["name"]
 
     
-    st.markdown(""" 
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    #GithubIcon {
-      visibility: hidden;
-    }
-    """, unsafe_allow_html=True)
-    
+    hide_github_icon = """
+
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+    """
+    st.markdown(hide_github_icon, unsafe_allow_html=True)
     
     
     # --- CONNECT TO DETA ---
