6655a634dc2e5cc4651eb55abc6f3b51526423c2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-15 11:17:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ elif st.session_state["authentication_status"]:
     drive = deta.Drive("df_pictures")
 
     # --- DIMENSIONS ---
-    OUTPUT_height = 550
+    OUTPUT_height = 560
     CONTAINER_height = 590
     # --- FUNCTIONS ---
     
