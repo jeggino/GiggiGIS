1c337279421c41a275a5557129bd9d78a27972ea
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-15 12:51:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -345,32 +345,20 @@ elif st.session_state["authentication_status"]:
 
 
 "---"
-
 import streamlit as st
+import pandas as pd
 
-def display_input_row(index):
-    left, middle, right = st.columns(3)
-    left.text_input('First', key=f'first_{index}')
-    middle.text_input('Middle', key=f'middle_{index}')
-    right.text_input('Last', key=f'last_{index}')
+if 'name' not in st.session_state:
+    st.session_state['name'] = 'John Doe'
 
-if 'rows' not in st.session_state:
-    st.session_state['rows'] = 0
+st.header(st.session_state['name'])
 
-def increase_rows():
-    st.session_state['rows'] += 1
+if st.button('Jane'):
+    st.session_state['name'] = 'Jane Doe'
+    st.rerun()
 
-st.button('Add person', on_click=increase_rows)
+if st.button('John'):
+    st.session_state['name'] = 'John Doe'
+    st.rerun()
 
-for i in range(st.session_state['rows']):
-    display_input_row(i)
-
-# Show the results
-st.subheader('People')
-for i in range(st.session_state['rows']):
-    st.write(
-        f'Person {i+1}:',
-        st.session_state[f'first_{i}'],
-        st.session_state[f'middle_{i}'],
-        st.session_state[f'last_{i}']
-    )
+st.header(st.session_state['name'])
