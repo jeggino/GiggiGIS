9b4dbf6dab9bc88f19c04afc1e495ae860da26f9
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-15 12:25:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ elif st.session_state["authentication_status"]:
     reduce_header_height_style = """
     <style>
        
-        div.block-container {padding-top:0rem; margin-top: -2em; padding-bottom: 0em; padding-left: 0rem; padding-right: 0rem;}
+        div.block-container {padding-top:0rem; padding-bottom: 0em; padding-left: 0rem; padding-right: 0rem; margin-top: -2em; margin-bottom: -2em;}
     </style>
     """
     st.markdown(reduce_header_height_style, unsafe_allow_html=True)
