1ad967d334dc35ab007936c55411e7394be46bf0
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-15 09:30:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,17 +50,16 @@ elif st.session_state["authentication_status"]:
 
     
    st.markdown(
-        """
-        <style>
-        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-        .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK {
-            display: none;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True)
     
     
     # --- CONNECT TO DETA ---
