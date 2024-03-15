436306abdba550258f7d0a89fe3009983cdf2bfd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-15 12:04:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ elif st.session_state["authentication_status"]:
     
     st.markdown("""
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { display: hidden; }
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
     </style>
     """,
     unsafe_allow_html=True)
