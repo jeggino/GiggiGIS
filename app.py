b5ee7b5b65d5c20d04e70eaf42e9dc75b225cdad
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-18 11:30:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ elif st.session_state["authentication_status"]:
                                           popup=popup,
                                           icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg_2)
                             
-                        elif df_2.iloc[i]['sp'] == "Gierzwaluwen":
+                        elif df_2.iloc[i]['sp'] == "Huismus":
                             
                             folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
                                           popup=popup,
