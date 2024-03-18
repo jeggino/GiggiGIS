ff75dc003af1d884d95dbb17125b1b5127170103
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-18 13:10:15 +0100

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -27,239 +27,205 @@ st.set_page_config(
     
 )
 
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+# --- CONNECT TO DETA ---
+deta = Deta(st.secrets["deta_key"])
+db = deta.Base("df_observations")
+drive = deta.Drive("df_pictures")
 
-authenticator.login()
+# --- DIMENSIONS ---
+OUTPUT_height = 610
+OUTPUT_width = 350
+CONTAINER_height = 640
+# --- FUNCTIONS ---
 
-if st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
-elif st.session_state["authentication_status"]:
-    
-    waarnemer = st.session_state["name"]
+def load_dataset():
+    return db.fetch().items
 
-    reduce_header_height_style = """
-    <style>
-        div.block-container {padding-top:0rem; padding-bottom: 0em; padding-left: 0rem; padding-right: 0rem; margin-top: -2em; margin-bottom: 2em;}
-    </style>
-    """
-    st.markdown(reduce_header_height_style, unsafe_allow_html=True)
-    
-    st.markdown("""
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-    </style>
-    """,
-    unsafe_allow_html=True)
-    
-    # --- CONNECT TO DETA ---
-    deta = Deta(st.secrets["deta_key"])
-    db = deta.Base("df_observations")
-    drive = deta.Drive("df_pictures")
+def insert_json(key,waarnemer,datum,soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,onbewoond,coordinates):
 
-    # --- DIMENSIONS ---
-    OUTPUT_height = 610
-    OUTPUT_width = 350
-    CONTAINER_height = 640
-    # --- FUNCTIONS ---
-    
-    def load_dataset():
-        return db.fetch().items
-    
-    def insert_json(key,waarnemer,datum,soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,onbewoond,coordinates):
-
-        return db.put({"key":key, "waarnemer":waarnemer,"datum":datum,"soortgroup":soortgroup, "aantal":aantal,
-                       "sp":sp, "gedrag":gedrag, "functie":functie, "verblijf":verblijf,
-                       "geometry_type":geometry_type,"lat":lat,"lng":lng,"opmerking":opmerking,"onbewoond":onbewoond,"coordinates":coordinates})
-            
-    
-    def map():
+    return db.put({"key":key, "waarnemer":waarnemer,"datum":datum,"soortgroup":soortgroup, "aantal":aantal,
+                   "sp":sp, "gedrag":gedrag, "functie":functie, "verblijf":verblijf,
+                   "geometry_type":geometry_type,"lat":lat,"lng":lng,"opmerking":opmerking,"onbewoond":onbewoond,"coordinates":coordinates})
         
-        m = folium.Map()
-        Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
-        Fullscreen().add_to(m)
-        LocateControl(auto_start=True).add_to(m)
-        
-        output = st_folium(m, returned_objects=["all_drawings"],width=OUTPUT_width, height=OUTPUT_height)
-        
-        return  output
-    
-    
-    def input_data():
 
-        with st.container(height=CONTAINER_height, border=True):
-            
-            output = map()
+def map():
+    
+    m = folium.Map()
+    Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False}).add_to(m)
+    Fullscreen().add_to(m)
+    LocateControl(auto_start=True).add_to(m)
+    
+    output = st_folium(m, returned_objects=["all_drawings"],width=OUTPUT_width, height=OUTPUT_height)
+    
+    return  output
+
+
+def input_data():
+
+    with st.container(height=CONTAINER_height, border=True):
         
-        with st.sidebar:
-            
-            submitted = st.button("Gegevens opslaan")
-            
-            if submitted:           
+        output = map()
     
-                try:
-    
-                    output["features"] = output.pop("all_drawings")
-                    geometry_type = output["features"][0]["geometry"]["type"]
-                    coordinates = output["features"][0]["geometry"]["coordinates"] 
+    with st.sidebar:
+        
+        submitted = st.button("Gegevens opslaan")
+        
+        if submitted:           
+
+            try:
+
+                output["features"] = output.pop("all_drawings")
+                geometry_type = output["features"][0]["geometry"]["type"]
+                coordinates = output["features"][0]["geometry"]["coordinates"] 
+                
+                if geometry_type == "LineString":
                     
-                    if geometry_type == "LineString":
-                        
-                        lng = None
-                        lat = None
-                        key = None
+                    lng = None
+                    lat = None
+                    key = None
+                
+                else: 
                     
-                    else: 
-                        
-                        lng = coordinates[0]
-                        lat = coordinates[1]
-                        coordinates = None
-                        
-                        key = str(lng)+str(lat)
-    
-                    if len(output["features"]) > 1:
-                        st.error("U kunt niet meer dan één waarneming tegelijk uploaden!")
-                        st.stop()
-    
+                    lng = coordinates[0]
+                    lat = coordinates[1]
+                    coordinates = None
+                    
+                    key = str(lng)+str(lat)
+
+                if len(output["features"]) > 1:
+                    st.error("U kunt niet meer dan één waarneming tegelijk uploaden!")
+                    st.stop()
+
+                else:
+
+                    if uploaded_file is not None:
+                        bytes_data = uploaded_file.getvalue()
+                        drive.put(f"{key}.jpeg", data=bytes_data)            
+                        with st.spinner('Wait for it...'):
+                            insert_json(key,waarnemer,str(datum),GROUP_DICT[soortgroup],aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,onbewoond,coordinates)
+                    
                     else:
-    
-                        if uploaded_file is not None:
-                            bytes_data = uploaded_file.getvalue()
-                            drive.put(f"{key}.jpeg", data=bytes_data)            
-                            with st.spinner('Wait for it...'):
-                                insert_json(key,waarnemer,str(datum),GROUP_DICT[soortgroup],aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,onbewoond,coordinates)
-                        
-                        else:
-                            with st.spinner('Wait for it...'):
-                                insert_json(key,waarnemer,str(datum),GROUP_DICT[soortgroup],aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,onbewoond,coordinates)
-    
-                        st.success('Gegevens opgeslagen!', icon="✅")
-                        
-                        
-                        
-    
-                except:
-                    st.info("Markeer een waarneming")
+                        with st.spinner('Wait for it...'):
+                            insert_json(key,waarnemer,str(datum),GROUP_DICT[soortgroup],aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,onbewoond,coordinates)
 
-                st.switch_page("app.py")
+                    st.success('Gegevens opgeslagen!', icon="✅")
+                    
+                    
+                    
 
+            except:
+                st.info("Markeer een waarneming")
+
+            st.switch_page("app.py")
+
+    
+
+
+def popup_html(row):
+    
+    i = row
+     
+    datum=df_2['datum'].iloc[i] 
+    soortgroup=df_2['soortgroup'].iloc[i]
+    sp = df_2['sp'].iloc[i] 
+    functie=df_2['functie'].iloc[i]
+    gedrag=df_2['gedrag'].iloc[i]
+    verblijf=df_2['verblijf'].iloc[i]
+    bewoond=df_2['onbewoond'].iloc[i] 
+    opmerking=df_2['opmerking'].iloc[i]
+    aantal=df_2['aantal'].iloc[i]
+    waarnemer=df_2['waarnemer'].iloc[i] 
+       
+
+    left_col_color = "#19a7bd"
+    right_col_color = "#f2f0d3"
+    
+    html = """<!DOCTYPE html>
+    <html>
+    <table style="height: 126px; width: 300;">
+    <tbody>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soortgroup</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(soortgroup) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soort</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(sp) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Gedrag</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(gedrag) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Bewoond</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(bewoond) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Aantal</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(aantal) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
+    </tr>
+    </tbody>
+    </table>
+    </html>
+    """
+    return html
         
-    
-    
-    def popup_html(row):
-        
-        i = row
-         
-        datum=df_2['datum'].iloc[i] 
-        soortgroup=df_2['soortgroup'].iloc[i]
-        sp = df_2['sp'].iloc[i] 
-        functie=df_2['functie'].iloc[i]
-        gedrag=df_2['gedrag'].iloc[i]
-        verblijf=df_2['verblijf'].iloc[i]
-        bewoond=df_2['onbewoond'].iloc[i] 
-        opmerking=df_2['opmerking'].iloc[i]
-        aantal=df_2['aantal'].iloc[i]
-        waarnemer=df_2['waarnemer'].iloc[i] 
-           
-    
-        left_col_color = "#19a7bd"
-        right_col_color = "#f2f0d3"
-        
-        html = """<!DOCTYPE html>
-        <html>
-        <table style="height: 126px; width: 300;">
-        <tbody>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soortgroup</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(soortgroup) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soort</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(sp) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Gedrag</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(gedrag) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Bewoond</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(bewoond) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Aantal</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(aantal) + """
-        </tr>
-        <tr>
-        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
-        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
-        </tr>
-        </tbody>
-        </table>
-        </html>
-        """
-        return html
-            
-    
-    # --- APP ---    
+
+# --- APP ---    
 
 with st.sidebar:
 
-    soortgroup = st.selectbox("", GROUP)
-    datum = st.date_input("Datum")        
+soortgroup = st.selectbox("", GROUP)
+datum = st.date_input("Datum")        
 
-    if soortgroup == '🦇 Vleermuizen':
+if soortgroup == '🦇 Vleermuizen':
 
-        sp = st.selectbox("Soort", BAT_NAMES)
-        gedrag = st.selectbox("Gedrag", BAT_BEHAVIOURS) 
-        functie = st.selectbox("Functie", BAT_FUNCTIE) 
-        verblijf = st.selectbox("Verblijf", BAT_VERBLIJF) 
-        onbewoond = None
+    sp = st.selectbox("Soort", BAT_NAMES)
+    gedrag = st.selectbox("Gedrag", BAT_BEHAVIOURS) 
+    functie = st.selectbox("Functie", BAT_FUNCTIE) 
+    verblijf = st.selectbox("Verblijf", BAT_VERBLIJF) 
+    onbewoond = None
 
-    elif soortgroup == '🪶 Vogels':
+elif soortgroup == '🪶 Vogels':
 
-        sp = st.selectbox("Soort", BIRD_NAMES)
-        gedrag = st.selectbox("Gedrag", BIRD_BEHAVIOURS) 
-        functie = st.selectbox("Functie", BIRD_FUNCTIE) 
-        verblijf = st.selectbox("Verblijf", BIRD_VERBLIJF) 
-        onbewoond = None
+    sp = st.selectbox("Soort", BIRD_NAMES)
+    gedrag = st.selectbox("Gedrag", BIRD_BEHAVIOURS) 
+    functie = st.selectbox("Functie", BIRD_FUNCTIE) 
+    verblijf = st.selectbox("Verblijf", BIRD_VERBLIJF) 
+    onbewoond = None
 
-    elif soortgroup == '🏠 Vleermuiskast':
-        onbewoond = st.selectbox("Bewoond", ["Ja","Nee"])
-        BAT_NAMES = ["onbekend"] + BAT_NAMES
-        sp = st.selectbox("Soort", BAT_NAMES) 
-        gedrag = None
-        functie = None
-        verblijf = None
-        
-
-    aantal = st.number_input("Aantal", min_value=0)
-    opmerking = st.text_input("", placeholder="Vul hier een opmerking in ...")
+elif soortgroup == '🏠 Vleermuiskast':
+    onbewoond = st.selectbox("Bewoond", ["Ja","Nee"])
+    BAT_NAMES = ["onbekend"] + BAT_NAMES
+    sp = st.selectbox("Soort", BAT_NAMES) 
+    gedrag = None
+    functie = None
+    verblijf = None
     
-    with st.expander("Upload een foto"):
-        uploaded_file = st.camera_input("")
+
+aantal = st.number_input("Aantal", min_value=0)
+opmerking = st.text_input("", placeholder="Vul hier een opmerking in ...")
+
+with st.expander("Upload een foto"):
+    uploaded_file = st.camera_input("")
 
 
-    
+
 input_data()
