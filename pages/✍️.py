84230b5cc079135c7f54419684b273c92b28b404
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-20 08:58:55 +0100

    Update ✍️.py

--- pages/✍️.py
+++ pages/✍️.py
@@ -35,12 +35,12 @@ reduce_header_height_style = """
 """
 st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
-# st.markdown("""
-# <style>
-# .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-# </style>
-# """,
-# unsafe_allow_html=True)
+st.markdown("""
+<style>
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+</style>
+""",
+unsafe_allow_html=True)
 
 
 # --- CONNECT TO DETA ---
@@ -50,6 +50,7 @@ drive = deta.Drive("df_pictures")
 
 try:
     waarnemer = st.session_state["name"]
+    
 except:
     st.switch_page("app.py")
 
