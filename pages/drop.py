e6e54ec53e6e16a1049caf0e9e5fdf806993a79a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-21 11:50:33 +0100

    Update drop.py

--- pages/drop.py
+++ pages/drop.py
@@ -27,16 +27,7 @@ st.set_page_config(
     
 )
 
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
 
 st.markdown("""
     <style>
