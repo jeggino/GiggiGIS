675da2e588a2dbca834b6d84b37b5e3ba7851d1a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-21 11:45:20 +0100

    Update drop.py

--- pages/drop.py
+++ pages/drop.py
@@ -144,112 +144,114 @@ def popup_html(row):
     
     
 try:
-      
-      db_content = load_dataset()
-      df_point = pd.DataFrame(db_content)
-          
-      df_2 = df_point
-      
-      df_2["icon_data"] = df_2.apply(lambda x: ICON[x["sp"]] if x["soortgroup"]=="Vogels" 
-                                     else (ICON["Bat"] if x["soortgroup"]=="Vleermuizen"  
-                                           else (ICON["Nest_bezet"] if x["onbewoond"]=="Ja" 
-                                                 else ICON["Nest_unbezet"])), axis=1)
-      
-      map = folium.Map()
-      LocateControl(auto_start=True).add_to(map)
-      Fullscreen().add_to(map)
-      
-      fg = folium.FeatureGroup(name="Vleermuiskast")
-      fg_2 = folium.FeatureGroup(name="Huismussen")
-      fg_4 = folium.FeatureGroup(name="Gierzwaluwen")
-      fg_3 = folium.FeatureGroup(name="Vleermuizen")
-      map.add_child(fg)
-      map.add_child(fg_2)
-      map.add_child(fg_4)
-      map.add_child(fg_3)
-      folium.LayerControl().add_to(map)
-     
 
-      
-      
-      for i in range(len(df_2)):
-
-          if df_2.iloc[i]['geometry_type'] == "Point":
-
-              html = popup_html(i)
-              popup = folium.Popup(folium.Html(html, script=True), max_width=300)
-
-              if df_2.iloc[i]['soortgroup'] == "Vleermuiskast":
-                  folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
-                                popup=popup,
-                                icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg)
-
-              elif df_2.iloc[i]['soortgroup'] == "Vogels":
-                  if df_2.iloc[i]['sp'] == "Huismus":
-                      
-                      folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
-                                    popup=popup,
-                                    icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg_2)
-                      
-                  elif df_2.iloc[i]['sp'] == "Gierzwaluw":
-                      
-                      folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
-                                    popup=popup,
-                                    icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg_4)
-
-
-              else:
-                  folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
-                                popup=popup,
-                                icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg_3)
-                  
-
-          elif df_2.iloc[i]['geometry_type'] == "LineString":
-
-              folium.PolyLine(df_2.iloc[i]['coordinates']).add_to(fg)
-
-      with st.container(height=CONTAINER_height, border=True):
-          output_2 = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,feature_group_to_add=[fg,fg_2,fg_3,fg_4])
-          
-      try:
-          
-          id = str(output_2["last_active_drawing"]['geometry']['coordinates'][0])+str(output_2["last_active_drawing"]['geometry']['coordinates'][1])
-          name = f"{id}.jpeg"
-  
-
-              
-
-        try:
-
-            res = drive.get(name).read()
-
+    db_content = load_dataset()
+    df_point = pd.DataFrame(db_content)
+    
+    df_2 = df_point
+    
+    df_2["icon_data"] = df_2.apply(lambda x: ICON[x["sp"]] if x["soortgroup"]=="Vogels" 
+             else (ICON["Bat"] if x["soortgroup"]=="Vleermuizen"  
+                   else (ICON["Nest_bezet"] if x["onbewoond"]=="Ja" 
+                         else ICON["Nest_unbezet"])), axis=1)
+    
+    map = folium.Map()
+    LocateControl(auto_start=True).add_to(map)
+    Fullscreen().add_to(map)
+    
+    fg = folium.FeatureGroup(name="Vleermuiskast")
+    fg_2 = folium.FeatureGroup(name="Huismussen")
+    fg_4 = folium.FeatureGroup(name="Gierzwaluwen")
+    fg_3 = folium.FeatureGroup(name="Vleermuizen")
+    map.add_child(fg)
+    map.add_child(fg_2)
+    map.add_child(fg_4)
+    map.add_child(fg_3)
+    folium.LayerControl().add_to(map)
+    
+    
+    
+    
+    for i in range(len(df_2)):
+    
+        if df_2.iloc[i]['geometry_type'] == "Point":
+        
+            html = popup_html(i)
+            popup = folium.Popup(folium.Html(html, script=True), max_width=300)
+            
+            if df_2.iloc[i]['soortgroup'] == "Vleermuiskast":
+                folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
+                    popup=popup,
+                    icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg)
+            
+            elif df_2.iloc[i]['soortgroup'] == "Vogels":
+                if df_2.iloc[i]['sp'] == "Huismus":
                 
-            with st.form("entry_form", clear_on_submit=True):
-                submitted = st.form_submit_button("Verwijder data")
-                if submitted:
-                    if waarnemer ==  df_point.set_index("key").loc[id,"waarnemer"]:
-                        db.delete(id)
-                        drive.delete(name)
-                        st.success('Gegevens verwijderd!', icon="✅")
-                        st.page_link("🗺️_Home.py", label="vernieuwen", icon="🔄")
-                    else:
-                        st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
-                        
-
+                    folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
+                            popup=popup,
+                            icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg_2)
+                
+                elif df_2.iloc[i]['sp'] == "Gierzwaluw":
+                
+                    folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
+                            popup=popup,
+                            icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg_4)
+            
+            
+            else:
+                folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
+                    popup=popup,
+                    icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=(30,30))).add_to(fg_3)
+            
+        
+        elif df_2.iloc[i]['geometry_type'] == "LineString":
+        
+            folium.PolyLine(df_2.iloc[i]['coordinates']).add_to(fg)
+            
+            with st.container(height=CONTAINER_height, border=True):
+            output_2 = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,feature_group_to_add=[fg,fg_2,fg_3,fg_4])
+    
+    try:
+    
+        id = str(output_2["last_active_drawing"]['geometry']['coordinates'][0])+str(output_2["last_active_drawing"]['geometry']['coordinates'][1])
+        name = f"{id}.jpeg"
+    
+    
+    
+    
+        try:
+        
+        res = drive.get(name).read()
+        
+        
+        with st.form("entry_form", clear_on_submit=True):
+            submitted = st.form_submit_button("Verwijder data")
+            
+        if submitted:
+            if waarnemer ==  df_point.set_index("key").loc[id,"waarnemer"]:
+                db.delete(id)
+                drive.delete(name)
+                st.success('Gegevens verwijderd!', icon="✅")
+                st.switch_page("🗺️_Home.py")
+            
+            else:
+                st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
+    
+    
         except:
             st.info('Geen foto opgeslagen voor deze waarneming!')
             with st.form("entry_form", clear_on_submit=True):
                 submitted = st.form_submit_button("Verwijder data")
-                if submitted:
-                    if waarnemer == df_point.set_index("key").loc[id,"waarnemer"]:
-                        db.delete(id)
-                        st.success('Gegevens verwijderd!', icon="✅")
-                        st.page_link("🗺️_Home.py", label="vernieuwen", icon="🔄")
-                    else:
-                        st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
+            if submitted:
+                if waarnemer == df_point.set_index("key").loc[id,"waarnemer"]:
+                    db.delete(id)
+                    st.success('Gegevens verwijderd!', icon="✅")
+                    st.switch_page("🗺️_Home.py")
+                else:
+                    st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
+    
+    except:
+        st.stop()
 
-      except:
-          st.stop()
-
-  except:
-      st.stop()
+except:
+    st.stop()
