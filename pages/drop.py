27757e951d37696568df2ab4049d3e12ab2dae26
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-21 11:47:11 +0100

    Update drop.py

--- pages/drop.py
+++ pages/drop.py
@@ -227,28 +227,28 @@ try:
         with st.form("entry_form", clear_on_submit=True):
             submitted = st.form_submit_button("Verwijder data")
             
-        if submitted:
-            if waarnemer ==  df_point.set_index("key").loc[id,"waarnemer"]:
-                db.delete(id)
-                drive.delete(name)
-                st.success('Gegevens verwijderd!', icon="✅")
-                st.switch_page("🗺️_Home.py")
-            
-            else:
-                st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
+            if submitted:
+                if waarnemer ==  df_point.set_index("key").loc[id,"waarnemer"]:
+                    db.delete(id)
+                    drive.delete(name)
+                    st.success('Gegevens verwijderd!', icon="✅")
+                    st.switch_page("🗺️_Home.py")
+                
+                else:
+                    st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
     
     
         except:
             st.info('Geen foto opgeslagen voor deze waarneming!')
             with st.form("entry_form", clear_on_submit=True):
                 submitted = st.form_submit_button("Verwijder data")
-            if submitted:
-                if waarnemer == df_point.set_index("key").loc[id,"waarnemer"]:
-                    db.delete(id)
-                    st.success('Gegevens verwijderd!', icon="✅")
-                    st.switch_page("🗺️_Home.py")
-                else:
-                    st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
+                if submitted:
+                    if waarnemer == df_point.set_index("key").loc[id,"waarnemer"]:
+                        db.delete(id)
+                        st.success('Gegevens verwijderd!', icon="✅")
+                        st.switch_page("🗺️_Home.py")
+                    else:
+                        st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
     
     except:
         st.stop()
