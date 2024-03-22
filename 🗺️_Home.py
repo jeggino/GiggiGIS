ba46c96ca7665faaf29f84f5df115137fec5e005
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-22 12:46:27 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -150,6 +150,7 @@ elif st.session_state["authentication_status"]:
     # --- APP ---    
 
     st.page_link("pages/drop.py", label="Drop", icon="❌")
+
     
     
     on = st.toggle('✍️ Voeg een waarneming in')
