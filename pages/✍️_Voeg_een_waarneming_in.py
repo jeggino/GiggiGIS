22947f23b0f959ed4a815288fc0f2e1d8188a9a5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-25 07:55:08 +0100

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -217,7 +217,7 @@ opmerking = popover.text_input("", placeholder="Vul hier een opmerking in ...")
 with popover.expander("Upload een foto"):
     uploaded_file = st.camera_input("")
     
-popover.page_link("🗺️_Home.py", label="Annuleren", icon="❌")
+st.page_link("🗺️_Home.py", label="Annuleren", icon="❌")
 
 input_data()
 
