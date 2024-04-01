64153cbfc84374a7ba7072f8df3bfeac3d282d2f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-04-01 10:14:03 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
 import folium
-from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
+from folium.plugins import Draw, Fullscreen, LocateControl
 from streamlit_folium import st_folium
 
 import pandas as pd
 import geopandas as gpd
 import datetime
 from datetime import date
-import random
 
 from deta import Deta
 
