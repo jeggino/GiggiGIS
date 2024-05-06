ea2e597874364837fb001daeb21a8f635ef7eec1
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-06 08:49:37 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -62,7 +62,6 @@ elif st.session_state["authentication_status"] is None:
 elif st.session_state["authentication_status"]:
 
     waarnemer = st.session_state.name
-    st.write(waarnemer)
 
     if waarnemer == 'Luigi Giugliano':
         deta = Deta(st.secrets[f"deta_key_jobert"])
