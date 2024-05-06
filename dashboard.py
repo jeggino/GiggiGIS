4a1489c86f32aa9342b3b824392ebb2752377753
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-07 00:44:55 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -47,5 +47,3 @@ def load_dataset():
 db_content = pd.DataFrame(load_dataset())
 
 st.dataframe(data=db_content, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
-
-st.write(db_content)
