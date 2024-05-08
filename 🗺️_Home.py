d991511956f5e2377568f212015c1ea352729726
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-08 08:24:50 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -19,25 +19,13 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-
+# ---LAYOUT---
 st.set_page_config(
     page_title="GigGIS",
     initial_sidebar_state="collapsed",
     page_icon="📝",
     layout="wide",
     
-)
-
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
 )
 
 st.markdown("""
@@ -64,6 +52,19 @@ st.markdown(
 </style>
 """,
     unsafe_allow_html=True,
+)
+
+# ---AUTENTICATION---
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
 )
 
 authenticator.login()
