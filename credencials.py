b93a12cc0b0f0b69625e260dbee58bdd52307d91
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-17 09:48:48 +0200

    Update credencials.py

--- credencials.py
+++ credencials.py
@@ -1,12 +1,12 @@
-# --- VARIABLES---
+# --- COSTANTS ---
 HELP_FUNCTIE = "Zomer- of kraamverblijfplaats: De vrouwtjes wonen in de zomer in kraamverblijfplaatsen. Hier brengt ze hun jongen groot. voorkomende leven ze gecombineerd in groepen (kolonies). Zomer- of mannenverblijfplaats: De mannetjes wonen in de zomer soms solitair, soms in groepen, maar altijd op een andere plaats dan de vrouwtjes van hun soort. Tijdelijke of paarverblijfplaats: Vaak kennen vleermuizen ook tussenkwartieren, waar ze slechts kort verblijven tijdens de reis van hun winter- naar zomerkolonie. Zo trekken zowel de mannetjes als de vrouwtjes aan het einde van de zomer naar speciale paarkwartieren, waar ze slechts kort verblijven. Winterverblijfplaats: Vleermuizen overwinteren in gebouwen, bunkers, ijskelders, groeven en boomholtes."
 
-GROUP = ["🪶 Vogels", "🦇 Vleermuizen", "🏠 Vleermuiskast"]
+GROUP = ["🦇 Vleermuizen","🪶 Vogels",  "🏠 Vleermuiskast"]
 
 GROUP_DICT = {"🪶 Vogels":"Vogels", "🦇 Vleermuizen":"Vleermuizen", "🏠 Vleermuiskast":"Vleermuiskast"}
 
-BAT_NAMES = ['Laatvlieger', 'Gewone dwergvleermuis', 'Watervleermuis',
-       'Rosse vleermuis', 'Ruige dwergvleermuis', 'Meervleermuis',
+BAT_NAMES = ['Gewone dwergvleermuis','Ruige dwergvleermuis', 'Laatvlieger', 'Rosse vleermuis', 'Watervleermuis',
+        'Meervleermuis',
        'Bosvleermuis', 'Franjestaart', 'Vleermuis onbekend',
        'Myotis spec.', 'Vale vleermuis', 'Gewone grootoorvleermuis',
        'Ingekorven vleermuis', 'Baardvleermuis', 'Brandts vleermuis',
@@ -14,36 +14,32 @@ BAT_NAMES = ['Laatvlieger', 'Gewone dwergvleermuis', 'Watervleermuis',
        'Bechsteins vleermuis', 'Tweekleurige vleermuis',
        'Dwergvleermuis spec.', 'Plecotus spec.', 'Mopsvleermuis']
 
-BIRD_NAMES = ['Gierzwaluw','Huismus']
+BAT_BEHAVIOURS = ['foeragerend', 'uitvliegend','invliegend', 'overvliegend', 
+         'zwermend', 'sporen', 'balts', 'verkeersslachtoffer']
 
-BAT_BEHAVIOURS = ['foeragerend', 'roepend','verplaatsend (vliegroute)', 'sociale roep', 'uitvliegend','invliegend', 'overvliegend', 
-           'nest-indicerend gedrag', 'zwermend', 'sporen', 'balts', 'verkeersslachtoffer','bezet nest']
-
-BIRD_BEHAVIOURS = ['overvliegend', 'bezet nest', 'nest-indicerend gedrag', 
-       'foeragerend', 'ter plaatse', 'roepend', 'baltsend / zingend',
-       'nestbouw', 'invliegend', 'uitvliegend', 'jagend',
-       'roepend vanuit gebouw', 'baltsend / zingend op gebouw',
-       'baltsend / zingend in vegetatie, struik of boom', 'sporen',
-       'geen / onbekend', 'onbekend', 'vondst', 'paaiend',
-       'verplaatsend (vliegroute)', 'copula']
-
-BAT_FUNCTIE = ['geen / onbekend', 'zomerverblijfplaats in gebouw', 'paarverblijfplaats in gebouw','vliegroute', 'kraamverblijfplaats in gebouw',
-           'vliegroute (bomen)', 'vliegroute (water)', 'zomerverblijfplaats in boom', 'paarverblijfplaats in boom', 
+BAT_FUNCTIE = ['geen / onbekend', 'zomerverblijfplaats in gebouw', 'paarverblijfplaats in gebouw',
+                'kraamverblijfplaats in gebouw',
+            'zomerverblijfplaats in boom', 'paarverblijfplaats in boom', 
            'kraamverblijfplaats in boom', 'winterverblijfplaats in gebouw', 'massa winterverblijfplaats', 
-           'essentieel foerageergebied (water)', 'winterverblijfplaats in bloei', 'essentieel foerageergebied (bomen)', 'vastgesteld territorium',
-           'essentieel foerageergebied (grasland)']
-
-BIRD_FUNCTIE = ['geen / onbekend', 'nestlocatie',  'vastgesteld territorium',
-       'functioneel leefgebied', 'mogelijke nestlocatie',
-       'voortplantingsbiotoop', 'winterverblijfplaats in boom',
-       'zomerverblijfplaats']
+           'winterverblijfplaats in bloei',]
 
 BAT_VERBLIJF = ['geen / onbekend', 'dakgoot', 'spouwmuur', 'daklijst',
        'kantpan', 'regenpijp', 'holte', 'raamkozijn', 'luik', 'scheur',
        'schoorsteen', 'gevelbetimmering', 'nokpan', 'dakpan',
        'vleermuiskast', 'openingen in dak', 'dakkapel', 'schors']
 
-VLEERMUISKAST_VERBLIJF = ["Op boom", "Op gebouw"]
+BIRD_NAMES = ['Gierzwaluw','Huismus']
+
+BIRD_BEHAVIOURS = ['overvliegend',  'nest-indicerend gedrag', 'foeragerend', 
+                    , 'invliegend', 'uitvliegend',
+       'roepend vanuit gebouw', 'baltsend / zingend op gebouw',
+       'baltsend / zingend in vegetatie, struik of boom', 'sporen',
+        'copula']
+
+
+
+BIRD_FUNCTIE = ['geen / onbekend', 'nestlocatie',  'vastgesteld territorium',
+       'mogelijke nestlocatie']
 
 BIRD_VERBLIJF = ['geen / onbekend', 'dakgoot', 'kantpan', 'zonnepaneel', 'nokpan', 'nestkast',
        'gevelbetimmering', 'openingen in dak', 'regenpijp',
@@ -51,6 +47,8 @@ BIRD_VERBLIJF = ['geen / onbekend', 'dakgoot', 'kantpan', 'zonnepaneel', 'nokpan
        'raamkozijn', 'luik', 'schoorsteen', 'daklijst', 'dakkapel',
        'in struweel / struiken', 'holte', 'op / bij nest in boom',
        'scheur', 'vleermuiskast']
+
+VLEERMUISKAST_VERBLIJF = ["Op boom", "Op gebouw"]
 
 ICON = {"Gierzwaluw":"https://cdn-icons-png.flaticon.com/128/732/732126.png",
         "Huismus":"https://cdn-icons-png.flaticon.com/128/8531/8531874.png",
