25d6e1004ef7a3fedba6a78c53c25bf87a053101
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-11 15:38:23 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -39,6 +39,8 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+st.html("<style> .main {overflow: hidden} </style>")
+
 st.markdown("""
     <style>
     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
