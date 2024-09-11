bcdabf1147bdfbd155eff9dd77cf8b5dbfca5dcd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-11 10:43:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,56 +7,56 @@ st.set_page_config(page_title='Streamlit', page_icon='🐍', initial_sidebar_sta
 
 users = fetch_users()
 users
-try:
-    users = fetch_users()
-    emails = []
-    usernames = []
-    passwords = []
+# try:
+users = fetch_users()
+emails = []
+usernames = []
+passwords = []
 
-    for user in users:
-        emails.append(user['key'])
-        usernames.append(user['username'])
-        passwords.append(user['password'])
+for user in users:
+    emails.append(user['key'])
+    usernames.append(user['username'])
+    passwords.append(user['password'])
 
-    credentials = {'usernames': {}}
-    for index in range(len(emails)):
-        credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
+credentials = {'usernames': {}}
+for index in range(len(emails)):
+    credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
 
-    Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='abcdef', cookie_expiry_days=4)
+Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='abcdef', cookie_expiry_days=4)
+st.warning(""PROBLEM!!!!!!!!!!!!!!!!")
+email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
 
-    email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
+info, info1 = st.columns(2)
 
-    info, info1 = st.columns(2)
+if not authentication_status:
+    sign_up()
 
-    if not authentication_status:
-        sign_up()
+if username:
+    if username in usernames:
+        if authentication_status:
+            # let User see app
+            st.sidebar.subheader(f'Welcome {username}')
+            Authenticator.logout('Log Out', 'sidebar')
 
-    if username:
-        if username in usernames:
-            if authentication_status:
-                # let User see app
-                st.sidebar.subheader(f'Welcome {username}')
-                Authenticator.logout('Log Out', 'sidebar')
+            st.subheader('This is the home page')
+            st.markdown(
+                """
+                ---
+                Created with ❤️ by SnakeByte
+                
+                """
+            )
 
-                st.subheader('This is the home page')
-                st.markdown(
-                    """
-                    ---
-                    Created with ❤️ by SnakeByte
-                    
-                    """
-                )
-
-            elif not authentication_status:
-                with info:
-                    st.error('Incorrect Password or username')
-            else:
-                with info:
-                    st.warning('Please feed in your credentials')
+        elif not authentication_status:
+            with info:
+                st.error('Incorrect Password or username')
         else:
             with info:
-                st.warning('Username does not exist, Please Sign up')
+                st.warning('Please feed in your credentials')
+    else:
+        with info:
+            st.warning('Username does not exist, Please Sign up')
 
 
-except:
-    st.success('Refresh Page')
+# except:
+#     st.success('Refresh Page')
