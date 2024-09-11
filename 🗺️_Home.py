55274d99e0dfc2c146d259e21e09628e7831060a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-11 21:34:29 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -58,7 +58,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="pages/✍️_Voeg_een_waarneming_in.py" target="_blank">gfgfgf</a>
+  <a class="navbar-brand" target="_blank"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
