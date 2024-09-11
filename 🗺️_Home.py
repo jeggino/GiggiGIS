1476f330648f4c95a554c8ead7cbf928657dc143
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-11 22:24:34 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -64,25 +64,6 @@ st.markdown("""
   </button>
 </nav>
 """, unsafe_allow_html=True)
-# st.markdown("""
-# <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #FFFFFF;">
-#   <a class="navbar-brand" target="_blank">   </a>
-#   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-#     <span class="navbar-toggler-icon"></span>
-#   </button>
-#   <div class="collapse navbar-collapse" id="navbarNav">
-#     <ul class="navbar-nav">
-#       <li class="nav-item active">
-#         <a class="nav-link disabled" href="#">🗺️<span class="sr-only">(current)</span></a>
-#       </li>
-#       <li class="nav-item">
-#         <a class="nav-link" href="pages/✍️_Voeg_een_waarneming_in.py" target="_blank">✍️</a>
-#       </li>
-#     </ul>
-#   </div>
-# </nav>
-# """, unsafe_allow_html=True)
-
 
 deta = Deta(st.secrets["deta_key_other"])
 db = deta.Base("df_observations")
