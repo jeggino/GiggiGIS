392a01cdcdc96d0afaacc9dbf7026e312b73d5a2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-11 13:53:54 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -228,11 +228,10 @@ try:
        
     df_2 = df_point
     
-    df_2["icon_data"] = df_2.apply(lambda x: ICON[x["sp"]] if ((x["soortgroup"]=="Vogels") & (x["functie"]!="nestlocatie")) 
-                                   else (ICON["Swift_nest"] if ((x["soortgroup"]=="Vogels") & (x["functie"]=="nestlocatie"))
-                                          else (ICON["Bat"] if x["soortgroup"]=="Vleermuizen"  
-                                             else (ICON["Nest_bezet"] if x["onbewoond"]=="Ja" 
-                                                   else ICON["Nest_unbezet"]))), axis=1)
+    df_2["icon_data"] = df_2.apply(lambda x: icon_dictionary[x["soortgroup"]][x["sp"]][x["functie"]] if x["soortgroup"] in ['Vogels','Vleermuizen'] 
+                                   else icon_dictionary[x["soortgroup"]][x["functie"]], 
+                                   axis=1
+                     )
     
     map = folium.Map()
     LocateControl(auto_start=True).add_to(map)
