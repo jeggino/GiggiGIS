21fee603e314f14b5e3a7f9def2affd3caad743a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-12 09:49:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,37 @@ for index in range(len(emails)):
 
 credentials 
 
-# Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='Authenticator', cookie_expiry_days=4)
-# st.warning("PROBLEM!!!!!!!!!!!!!!!!")
-# email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
 
-# info, info1 = st.columns(2)
+import streamlit as st
+
+@st.cache_resource()
+def logIn():
+    name = st.text_input("Insert your name", "")
+    password = st.text_input("Insert yput password", "")
+
+    if password == st.secret["password"]:
+       return True, name
+    
+    else:
+        return False
+
+# _____APP_____
+logIn()
+
+if logIn() == False:
+    st.error(f"password incorrect, {name}")
+    st.stop()
+
+st.write(f"Welkom {name}")
+
+@st.cache_resource()
+def logOut():
+    st.cache_resource.clear()
+    st.rerun()
+
+
+
+
 
 # if not authentication_status:
 #     sign_up()
