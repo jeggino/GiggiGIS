c4777cbac4a5eae3009b51e183f22f4492388ebf
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-12 12:40:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ def logIn():
 def project():
  project = st.selectbox("Chose a project",ASSAYS)
  if st.button("start"):
-  st.session_state.project = {"project_name": project}
-  st.rerun()
+      st.session_state.project = {"project_name": project}
+      st.rerun()
 
 def logOut():
     if st.button("logOut",use_container_width=True):
         del st.session_state.login
-     del st.session_state.project     
+        del st.session_state.project     
         st.rerun()
 
 def logOut_project():
@@ -51,14 +51,14 @@ if st.session_state.login['password'] != st.secrets['password']:
     st.stop()
 
 if 'project' not in st.session_state:  
- st.write("Chose a project")
- project()
- st.stop()
+    st.write("Chose a project")
+    project()
+    st.stop()
 
 with st.sidebar:
- f"Hello {st.session_state.login['name']} you will work at {st.session_state.project['project_name']} project"
- logOut_project()
- logOut()
+    f"Hello {st.session_state.login['name']} you will work at {st.session_state.project['project_name']} project"
+    logOut_project()
+    logOut()
 
 
 
