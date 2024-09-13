c89ccbc09a33bc6563a8fafba4ec3607f1d27cd7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-13 08:45:53 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -149,6 +149,57 @@ def popup_html(row):
     """
     return html
 
+#______________NEW___________________
+deta = Deta(st.secrets["deta_key_other"])
+db = deta.Base("df_observations")
+drive = deta.Drive("df_pictures")
+db_content = db.fetch().items 
+df_point = pd.DataFrame(db_content)
+SOORTGROUP = df_point.soortgroup.unique()
+
+def logIn():
+    name = st.text_input("Nane ...")
+    password = st.text_input("Password ...")
+    if st.button("logIn"):
+        st.session_state.login = {"name": name, "password": password}
+        st.rerun()
+
+def project():
+    project = st.selectbox("Chose a project",SOORTGROUP)
+    if st.button("start"):
+         st.session_state.project = {"project_name": project}
+         st.rerun()
+
+def logOut():
+    if st.button("logOut",use_container_width=True):
+        del st.session_state.login
+        del st.session_state.project     
+        st.rerun()
+
+def logOut_project():
+    if st.button("logOut project",use_container_width=True):
+        del st.session_state.project
+        st.rerun()
+        
+
+
+if "login" not in st.session_state:
+    st.write("LogIn please")
+    logIn()
+    st.stop()
+
+if st.session_state.login['password'] != st.secrets['password']:
+    f"Sorry {st.session_state.login['name']} your password is not correct"
+    logIn()
+    st.stop()
+
+if 'project' not in st.session_state:  
+    st.write("Chose a project")
+    project()
+    st.stop()
+
+#______________NEW___________________
+
 IMAGE = "image/logo.png"
 st.logo(IMAGE,  link=None, icon_image=None)
 
