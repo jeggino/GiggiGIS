111443524299f0c1db62716e7346fcab48dcb6d3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-13 10:20:00 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -219,9 +219,9 @@ try:
     
        
     df_2 = df_point[df_point['soortgroup']==st.session_state.project['project_name']]
-    df_2["datum"] = pd.to_datetime(df_2["datum"])
-    date_min = st.sidebar.write(df_2.datum.min()).dt.date
-    date_max = st.sidebar.write(df_2.datum.max()).dt.date
+    df_2["datum"] = pd.to_datetime(df_2["datum"]).dt.date
+    date_min = st.sidebar.write(df_2.datum.min())
+    date_max = st.sidebar.write(df_2.datum.max())
     d = st.sidebar.date_input(
         "Select your vacation for next year",
         (date_min, date_max),
