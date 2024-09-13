af80b2051781bab8a49faa7fb0d83df9df00985f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-13 10:17:38 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -227,7 +227,7 @@ try:
         (date_min, date_max),
         date_min,
         date_max,
-        # format="MM.DD.YYYY",
+        format="YYYY-MM_DD",
     )
     
     df_2["icon_data"] = df_2.apply(lambda x: icon_dictionary[x["soortgroup"]][x["sp"]][x["functie"]] if x["soortgroup"] in ['Vogels','Vleermuizen'] 
