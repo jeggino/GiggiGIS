5e8c7e1df36ce6bc7a2f7bf68de1a4b6afdb6e15
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-14 09:36:31 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -215,8 +215,8 @@ with st.sidebar:
     logOut_project()
     logOut()
     st.divider()
-    if st.botton("ciao",use_container_width=True):
-        report()
+if st.botton("ciao",use_container_width=True):
+    report()
     
     
 
