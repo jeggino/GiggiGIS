56364c6da298abf5421952b282c824adfff1897b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-17 11:38:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,68 +4,15 @@ import pandas as pd
 
 
 
-
-st.set_page_config(page_title='References', page_icon='🔐', initial_sidebar_state='collapsed')
-
-DICTIONARY_PROJECTS = {"Overing":["Vogels","Vleermuizen","Vleermuiskast"],
-                      "A-001":["Camera","Rat val"],
-                      "All":["Vogels","Vleermuizen","Vleermuiskast","Camera","Rat val"]}
-
 deta = Deta(st.secrets["deta_key_other"])
 db = deta.Base("df_authentication")
 db_content = db.fetch().items 
 df_references = pd.DataFrame(db_content)
 
-def logIn():
-    name = st.selectbox("Wie ben je?",df_references["username"].tolist())
-    
-    password = st.text_input("Vul het wachtwoord in, alstublieft")
-    index = df_references[df_references['username']==name].index[0]
-    true_password = df_references.loc[index,"password"]
-                             
-    if st.button("logIn"):
-        if password == true_password:
-            st.session_state.login = {"name": name, "password": password}
-            st.rerun()
-
-        else:
-            st.write("the password is not correct")
-
-def project():
-    index_project = df_references[df_references['username']==st.session_state.login["name"]].index[0]
-    project_list = df_references.loc[index_project,"project"]
-    project = st.selectbox("Aan welke project ga je werken?",project_list,label_visibility="visible")
-    opdracht = st.selectbox("Aan welke opdracht ga je werken?",DICTIONARY_PROJECTS[project],label_visibility="visible")
-    if st.button("begin"):
-         st.session_state.project = {"project_name": project,"opdracht": opdracht}
-         st.rerun()
-
-
-
-# ___APP___
-if "login" not in st.session_state:
-    logIn()
-    st.stop()
-
-if 'project' not in st.session_state:  
-    project()
-    st.stop()
-    
-st.markdown(f"""
-Hi **{st.session_state.login["name"]}**, 
-you are working at the **{st.session_state.project["project_name"]}** with the assignment **{st.session_state.project["opdracht"]}**
-""")
-
-df_references
-
-# if st.toggle("Do you want to update?"):
-#   update_item()
-
-
-import streamlit as st
 
 @st.dialog("Cast your vote")
 def update_item():
+  
   key_update = st.selectbox("chose a key to udate",df_references["key"].tolist(),label_visibility="visible")
   password_update = title = st.text_input("Write the new password")
 
@@ -75,6 +22,15 @@ def update_item():
     del st.session_state.login
     del st.session_state.project
     st.rerun()
+
+
+st.set_page_config(page_title='References', page_icon='🔐', initial_sidebar_state='collapsed')
+
+
+
+#___APP____
+
+df_references
 
 if st.button("Do you want to update?",use_container_width=True):
   update_item()
