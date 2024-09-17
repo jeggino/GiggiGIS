e97524a2364ef13ccd2c0404a13b54fd0f46de6f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-17 08:30:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 import streamlit as st
 from deta import Deta
 import pandas as pd
-import folium
-from folium.plugins import  Fullscreen, LocateControl, GroupedLayerControl
-from streamlit_folium import st_folium
 
-import geopandas as gpd
-import datetime
-from datetime import date
-import random
 
-from costants import *
-from functions import *
 
 
 st.set_page_config(page_title='References', page_icon='🔐', initial_sidebar_state='collapsed')
