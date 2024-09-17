92068714cefe18159fc6817ccf7eb556ec909000
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-17 08:46:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,26 @@ drive = deta.Drive("df_pictures")
 db_content = db.fetch().items 
 df_references = pd.DataFrame(db_content)
 df_references
+
+def logIn():
+    name = st.selectbox("Wie ben je?",df_references["username"].tolist())
+    
+    password = st.text_input("Vul het wachtwoord in, alstublieft")
+    index = df_references[df_references['username']=="name"].index[0]
+    true_password = df.loc[index,"project"]
+                             
+    if st.button("logIn"):
+        if password == true_password:
+            st.session_state.login = {"name": name, "password": password}
+            st.rerun()
+
+        else:
+            st.write("the password is not correct")
+
+
+# ___APP___
+if "login" not in st.session_state:
+    logIn()
+    st.stop()
+
+st.write("It's fine!!")
