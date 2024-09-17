9737f38e64966dc76f5c07568c84f268c54ec851
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-17 10:49:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -82,3 +82,23 @@ df_references
 
 if st.toggle("Do you want to update?"):
   update_item()
+
+
+import streamlit as st
+
+@st.dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
+
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
