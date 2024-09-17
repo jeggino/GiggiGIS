0211bfe797c5c96f9a502c0c36c3009017538482
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-17 09:01:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import pandas as pd
 
 st.set_page_config(page_title='References', page_icon='🔐', initial_sidebar_state='collapsed')
 
-
+DICTIONARY_PROJECTS = {"Overing":["Vogels","Vleermuizen","Vleermuiskast"],
+                      "A-001":["Camera","Rat val"],
+                      "ALL":["Vogels","Vleermuizen","Vleermuiskast","Camera","Rat val"]}
 
 deta = Deta(st.secrets["deta_key_other"])
 db = deta.Base("df_authentication")
@@ -31,10 +33,37 @@ def logIn():
         else:
             st.write("the password is not correct")
 
+def project():
+    index_project = df_references[df_references['username']==session_state.login.name].index[0]
+    project_list = df_references.loc[index,"project"]
+    project = st.selectbox("Aan welke project ga je werken?",project_list,label_visibility="visible")
+    opdracht = st.selectbox("Aan welke opdracht ga je werken?",DICTIONARY_PROJECTS[project],label_visibility="visible")
+    if st.button("begin"):
+         st.session_state.project = {"project_name": project,"opdracht": opdracht}
+         st.rerun()
+
+def logOut():
+    if st.button("logOut",use_container_width=True):
+        del st.session_state.login
+        del st.session_state.project     
+        st.rerun()
+
+def logOut_project():
+    if st.button("Opdracht wijzigen",use_container_width=True):
+        del st.session_state.project
+        st.rerun()
+
 
 # ___APP___
 if "login" not in st.session_state:
     logIn()
     st.stop()
 
-st.write("It's fine!!")
+if 'project' not in st.session_state:  
+    project()
+    st.stop()
+    
+st.markdown(f"""
+Hi **{session_state.login.name}**, 
+you are working at the **{st.session_state.project.project_name}** with the assignment **{st.session_state.project.opdracht}**
+""")
