ed0574de32cc67a21f13497cbd00de2ef97e40d5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-17 11:31:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,28 +40,6 @@ def project():
          st.session_state.project = {"project_name": project,"opdracht": opdracht}
          st.rerun()
 
-def logOut():
-    if st.button("logOut",use_container_width=True):
-        del st.session_state.login
-        del st.session_state.project     
-        st.rerun()
-
-def logOut_project():
-    if st.button("Opdracht wijzigen",use_container_width=True):
-        del st.session_state.project
-        st.rerun()
-
-# def update_item():
-#   key_update = st.selectbox("chose a key to udate",df_references["key"].tolist(),label_visibility="visible")
-#   password_update = title = st.text_input("Write the new password")
-
-#   update = {"password":password_update}
-#   if st.button("Update",use_container_width=True): 
-#     db.update(update,key_update)
-#     st.rerun()
-
-  
-  
 
 
 # ___APP___
@@ -94,10 +72,11 @@ def update_item():
   update = {"password":password_update}
   if st.button("Update",use_container_width=True): 
     db.update(update,key_update)
+    del st.session_state.login
+    del st.session_state.project
     st.rerun()
 
-on = st.toggle("Do you want to update?")
-if on:
+if st.toggle("Do you want to update?"):
   update_item()
     
 
