2f0e41f99e98175a4a4e3f426c165a047e73fdef
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-20 08:58:59 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -7,7 +7,7 @@ from streamlit_folium import st_folium
 import pandas as pd
 import geopandas as gpd
 import datetime
-from datetime import date
+from datetime import datetime, timedelta, date
 import random
 
 from deta import Deta
