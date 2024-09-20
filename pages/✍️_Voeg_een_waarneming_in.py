00d54ba78a343aa7021a41d544828e0eec1a3083
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-20 10:49:30 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -227,6 +227,8 @@ try:
         
         output_map = map()
 
+    output_map["features"][0]["geometry"]["type"]
+
     # if output_map["features"][0]["geometry"]["type"]:
     
     # input_data(map())
