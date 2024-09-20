74ce829946c46775a82c873c4595c39d8795c084
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-20 11:05:16 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -227,11 +227,11 @@ try:
         
         output_map = map()
     output_map["features"] = output_map.pop("all_drawings")
-    output_map#["features"]#["all_drawings"]
+    # output_map#["features"]#["all_drawings"]
     if len(output_map["features"]) != 0:
     # # if output_map["features"][0]["geometry"]["type"]:
         st.write("empty")
-    #     # input_data(map())
+        input_data(map())
     
 except:
     st.switch_page("🗺️_Home.py")
