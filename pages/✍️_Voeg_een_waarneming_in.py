dcf01a4fbcb9de12c0a603af72dde33b9f6aabfd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-21 08:36:39 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -46,7 +46,7 @@ st.markdown("""
 
 reduce_header_height_style = """
 <style>
-    div.block-container {padding-top: -1rem; padding-bottom: 0rem; padding-left: 0rem; padding-right: 0rem;}
+    div.block-container {padding-top: 0rem; padding-bottom: 0rem; padding-left: 0rem; padding-right: 0rem;}
 </style>
 """
 
