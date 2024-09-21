072168f9736c35de0f69eedbdeafb3dedcdee182
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-21 08:00:10 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -68,8 +68,8 @@ from streamlit_js_eval import streamlit_js_eval
 
 WIDTH_SCREEN = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
 HEIGHT_SCREEN = streamlit_js_eval(js_expressions='screen.height', key = 'SCR1')
-st.write(f"Screen width is {WIDTH_SCREEN}")
-st.write(f"Screen height is {HEIGHT_SCREEN}")
+# st.write(f"Screen width is {WIDTH_SCREEN}")
+# st.write(f"Screen height is {HEIGHT_SCREEN}")
 OUTPUT_height = WIDTH_SCREEN
 OUTPUT_width = HEIGHT_SCREEN
 CONTAINER_height = WIDTH_SCREEN
