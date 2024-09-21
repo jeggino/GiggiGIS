05d8d1ddb3e14a3c6c57524a55e5d4fa307bdebb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-21 09:08:44 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_js_eval import streamlit_js_eval
 
 import folium
 from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
@@ -14,53 +15,41 @@ from deta import Deta
 
 from credencials import *
 
-#_____NEW CHANGES____
+
 # ---LAYOUT---
 st.set_page_config(
     page_title="GigGIS",
     initial_sidebar_state="collapsed",
     page_icon="📝",
-    layout="centered",
+    layout="wide",
     
 )
 
-st.markdown(
-    """
-<style>
-    [data-testid="collapsedControl"] {
-        display: none
-    }
-</style>
-""",
-    unsafe_allow_html=True,
-)
+
+WIDTH_SCREEN = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
+HEIGHT_SCREEN = streamlit_js_eval(js_expressions='screen.height', key = 'SCR1')
+OUTPUT_height = int(HEIGHT_SCREEN) * 0.75
+OUTPUT_width = WIDTH_SCREEN 
+ICON_SIZE = (20,20)
+ICON_SIZE_huismus = (28,28)
 
 st.markdown("""
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_ } 
     </style>
     """,
     unsafe_allow_html=True)
 
 reduce_header_height_style = """
 <style>
-    div.block-container {padding-top: 0rem; padding-bottom: 0rem; padding-left: 0rem; padding-right: 0rem; margin-top: 1em; margin-bottom: 2em;}
+    div.block-container {padding-top: 0rem; padding-bottom: 0rem; padding-left: 0rem; padding-right: 0rem;}  header { visibility: hidden; padding-top: 0rem}
 </style>
 """
 
 st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #FFFFFF;">
-  <a class="navbar-brand" target="_blank">   </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-</nav>
-""", unsafe_allow_html=True)
-
+# --- DATASET ---
 deta = Deta(st.secrets["deta_key_other"])
 db = deta.Base("df_observations")
 drive = deta.Drive("df_pictures")
@@ -73,16 +62,10 @@ db_content_2 = db_2.fetch().items
 df_references = pd.DataFrame(db_content_2)
 
 
-# --- DIMENSIONS ---
-OUTPUT_height = 610
-OUTPUT_width = 350
-CONTAINER_height = 640
-ICON_SIZE = (20,20)
-ICON_SIZE_huismus = (28,28)
 
-# --- FUNCTIONS ---
 
-    
+
+# --- FUNCTIONS ---    
 def load_dataset():
     return db.fetch().items
 
@@ -271,7 +254,6 @@ if 'project' not in st.session_state:
 
 
 with st.sidebar:
-    # st.markdown(f"Hallo **{st.session_state.login['name']}**, je gaat werken aan de **{st.session_state.project['project_name']}** project, met de **{st.session_state.project['opdracht']}** opdracht. :rainbow[VEEL SUCCES!!!]")
     logOut_project()
     logOut()
     st.divider()
