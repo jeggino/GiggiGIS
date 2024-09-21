f116eec2e545f875563cf417fe7e8261a4d00364
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-21 09:01:59 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -25,11 +25,11 @@ st.set_page_config(
     
 )
 
-# .viewerBadge_text__1JaDK{ display: none; }
+# .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; }
 
 st.markdown("""
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,  footer { visibility: hidden; } 
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_,   footer { visibility: hidden; } 
     </style>
     """,
     unsafe_allow_html=True)
