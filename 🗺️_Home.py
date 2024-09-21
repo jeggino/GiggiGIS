255b11bb21f0214c10c2d84a1e786510d6490929
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-21 09:35:32 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -28,7 +28,7 @@ st.set_page_config(
 
 WIDTH_SCREEN = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
 HEIGHT_SCREEN = streamlit_js_eval(js_expressions='screen.height', key = 'SCR1')
-OUTPUT_height = HEIGHT_SCREEN * 0.75
+OUTPUT_height = HEIGHT_SCREEN / 0.75
 OUTPUT_width = WIDTH_SCREEN
 ICON_SIZE = (20,20)
 ICON_SIZE_huismus = (28,28)
