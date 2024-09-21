22dc238027c873394253d16cb8cdb10d3d1bdb90
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-21 09:11:36 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -340,9 +340,8 @@ try:
 
             folium.PolyLine(df_2.iloc[i]['coordinates']).add_to(fg)
 
-    with st.container(height=CONTAINER_height, border=True):
-        output_2 = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,
-                             feature_group_to_add=list(functie_dictionary.values()))
+    output_2 = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,
+                         feature_group_to_add=list(functie_dictionary.values()))
         
     try:
         
