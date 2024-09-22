2e67b248535e2c5f56eb27236a7e5c95fcdf0fc9
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 12:18:17 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -38,7 +38,7 @@ st.markdown(
 
 st.markdown("""
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } github { visibility: hidden; }footer { visibility: hidden; } header { visibility: True; }
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } GithubIcon { visibility: hidden; }footer { visibility: hidden; } header { visibility: True; }
     </style>
     """,
     unsafe_allow_html=True)
