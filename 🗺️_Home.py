e4ff04084fca6a4a81761e46540fb35aca134290
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 11:20:10 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -68,8 +68,8 @@ drive = deta.Drive("df_pictures")
 
 
 # --- DIMENSIONS ---
-st.write(f"Screen width is _{streamlit_js_eval(js_expressions='screen.width',  key = 'width')}_")
-st.write(f"Screen height is _{streamlit_js_eval(js_expressions='screen.height', key = 'height')}_")
+st.write(f"Screen width is {streamlit_js_eval(js_expressions='screen.width',  key = 'width')}")
+st.write(f"Screen height is {streamlit_js_eval(js_expressions='screen.height', key = 'height')}")
 OUTPUT_height = 610
 OUTPUT_width = 350
 CONTAINER_height = 640
