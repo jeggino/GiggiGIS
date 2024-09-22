fd155f7380233937bb87809df2a863725a87eaa9
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 10:52:54 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -395,7 +395,7 @@ try:
             except:
                 st.info('Geen foto opgeslagen voor deze waarneming')
 
-                if st.button("Waarneming bijwerken",use_container_width=True):
+                if st.button(":red[Waarneming bijwerken]",use_container_width=True):
                     update_item()
                 
                 with st.form("entry_form", clear_on_submit=True,border=False):
@@ -403,7 +403,7 @@ try:
                     if submitted:
                     # if waarnemer == df_point.set_index("key").loc[id,"waarnemer"]:
                         db.delete(id)
-                        st.success('Waarneming verwijderd', icon="✅")         
+                        st.success(':red[Waarneming verwijderd]', icon="✅")         
                         st.page_link("🗺️_Home.py", label="vernieuwen", icon="🔄")
                             # else:
                             #     st.warning('Je kunt deze observatie niet uitwissen. Een andere gebruiker heeft het gemarkeerd.', icon="⚠️")
