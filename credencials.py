a4a94b4e9b40f5c730576fec033abcf9968651c8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 10:44:22 +0200

    Update credencials.py

--- credencials.py
+++ credencials.py
@@ -8,6 +8,8 @@ deta = Deta(st.secrets[f"deta_key_other"])
 db = deta.Base("df_observations")
 drive = deta.Drive("df_pictures") 
 
+db_2 = deta.Base("df_authentication")
+
 # --- COSTANTS ---
 IMAGE = "image/logo.png"
 
@@ -15,6 +17,9 @@ WIDTH_SCREEN = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
 HEIGHT_SCREEN = streamlit_js_eval(js_expressions='screen.height', key = 'SCR1')
 OUTPUT_WIDTH = WIDTH_SCREEN 
 OUTPUT_HEIGHT = HEIGHT_SCREEN
+
+ICON_SIZE = (20,20)
+ICON_SIZE_huismus = (28,28)
 
 # int(float(HEIGHT_SCREEN) * 0.75)
 
