a3118cd025d1a5027fc458dc8c58108aae8e7230
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 12:58:51 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -52,13 +52,8 @@ drive = deta.Drive("df_pictures")
 # --- DIMENSIONS ---
 innerWidth = streamlit_js_eval(js_expressions='screen.width',  want_output = True, key = 'width')
 innerHeight = streamlit_js_eval(js_expressions='window.screen.height', want_output = True, key = 'height')
-# st.write(f"Screen width is _{innerWidth}_")
-# st.write(f"Screen height is _{innerHeight}_")
-# # st.write(type(innerHeight))
-# st.write(int(innerHeight * 0.75))
 OUTPUT_width = innerWidth
 OUTPUT_height = innerHeight
-# CONTAINER_height = 640
 ICON_SIZE = (20,20)
 ICON_SIZE_huismus = (28,28)
 
