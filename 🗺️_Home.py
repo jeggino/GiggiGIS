560337f064770a76620bec5d02caccb31b3b8fc2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 12:30:42 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -33,8 +33,7 @@ st.markdown("""
     """,
     unsafe_allow_html=True)
 
-hide_github_icon = """#GithubIcon {visibility: hidden;}"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 reduce_header_height_style = """
 <style>
