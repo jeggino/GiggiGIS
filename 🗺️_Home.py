a85bbca0abe4068fe9ea2a8df84eb886b3317581
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 11:20:51 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit_js_eval
+from streamlit_js_eval import streamlit_js_eval
 
 import folium
 from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
