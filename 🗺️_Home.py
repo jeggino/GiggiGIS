22aaa08783ebf9aeaaf44b237797e65636698c52
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-22 11:48:45 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -36,12 +36,12 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# st.markdown("""
-#     <style>
-#     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-#     </style>
-#     """,
-#     unsafe_allow_html=True)
+st.markdown("""
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+    </style>
+    """,
+    unsafe_allow_html=True)
 
 reduce_header_height_style = """
 <style>
