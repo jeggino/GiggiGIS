f2420c5bbba14cf399a452a79e46ed9e369453f4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-23 10:35:25 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -37,7 +37,7 @@ st.markdown("""
 
 reduce_header_height_style = """
 <style>
-    div.block-container {padding-top: 0rem; padding-bottom: 0rem; padding-left: 0rem; padding-right: 0rem; margin-top: -2rem; margin-bottom: 0rem;}
+    div.block-container {padding-top: 1rem; padding-bottom: 0rem; padding-left: 0rem; padding-right: 0rem; margin-top: -2rem; margin-bottom: 0rem;}
 </style>
 """ 
 
