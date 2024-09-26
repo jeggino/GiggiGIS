5eac74153e224a41ad234223edcfc4c7ea71dddd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-27 01:17:53 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -249,10 +249,10 @@ try:
             folium.PolyLine(df_2.iloc[i]['coordinates']).add_to(fg)
 
         
-    col_1,col_2 = st.columns([11,4],gap="small") 
+    col_1,col_2,col_3 = st.columns([1,11,4],gap="small") 
     for metric in df_2["functie"].unique():
-        col_2.metric(f"{metric}", f"{len(df_2[df_2["functie"]==metric])}")
-    with col_1:
+        col_3.metric(f"{metric}", f"{len(df_2[df_2["functie"]==metric])}")
+    with col_2:
         output_2 = st_folium(map,returned_objects=["last_active_drawing"],
                              width=850, 
                              feature_group_to_add=list(functie_dictionary.values()))
