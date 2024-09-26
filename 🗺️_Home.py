d58d958d4c7349afcdbc900048cad6b86dda2e5f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-27 00:03:46 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -247,8 +247,8 @@ try:
         elif df_2.iloc[i]['geometry_type'] == "LineString":
 
             folium.PolyLine(df_2.iloc[i]['coordinates']).add_to(fg)
-
-    with st.container(width=1000, border=True):
+    col_1,col_2,col_3 = st.columns(3)
+    with col_2.container( border=True):
         output_2 = st_folium(map,returned_objects=["last_active_drawing"],
                              width=900, 
                              # height=OUTPUT_height,
