32d43cb37a25d02832224f3a519f3c2cfdc411d5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-26 23:32:10 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -187,7 +187,7 @@ try:
     
     df_2 = df_point[df_point['project']=="A-001"]
     soortgroup = st.selectbox("",("Camera", "Rat val"))
-    df_2 = df_point[df_point['soortgroup']=="soortgroup"]
+    df_2 = df_point[df_point['soortgroup']==soortgroup]
     df_2["datum_2"] = pd.to_datetime(df_2["datum"]).dt.date
     st.sidebar.subheader("Filter op",divider=False)
     d = st.sidebar.date_input(
