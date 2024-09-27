bab69075ff66ce16614f55b911a84eaaaf9f7bf5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-28 00:10:14 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -79,6 +79,7 @@ def popup_html(row):
 
     project=df_2['project'].iloc[i]
     datum=df_2['datum'].iloc[i] 
+    datum_2=df_2['datum_2'].iloc[i] 
     time=df_2['time'].iloc[i]
     verblijf=df_2['verblijf'].iloc[i]
     sp = df_2['sp'].iloc[i] 
@@ -109,6 +110,11 @@ def popup_html(row):
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
     <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
     </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum vervangen</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum_2) + """
+    </tr>
+    <tr>
     <tr>
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
     <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
