24e15698d8d1c1e03128476c544dc7c25ca23b83
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-28 07:58:21 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -68,7 +68,7 @@ def map():
     Draw(draw_options={'circle': False,'rectangle': False,'circlemarker': False, 'polyline': False, 'polygon': False,},
         position="topright",).add_to(m)
     Fullscreen(position="topright").add_to(m)
-    LocateControl(auto_start=True).add_to(m)
+    LocateControl(auto_start=True,position="topright").add_to(m)
     
 
     
