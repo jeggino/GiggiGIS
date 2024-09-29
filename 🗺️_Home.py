cd2fa53706d9e3fb6ffc1a452f0721a115b6364d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-29 11:17:18 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -194,7 +194,7 @@ try:
     df_point = pd.DataFrame(db_content)
     
     df_2 = df_point[df_point['project']=="Ratten Terschelling"]
-    soortgroup = st.sidebar.multiselect("",("📷 Camera", "🪤 Rat val", '𐂺 Vangkooi'),("📷 Camera"))
+    soortgroup = st.sidebar.multiselect("",("📷 Camera", "🪤 Rat val", '𐂺 Vangkooi'),("📷 Camera", "🪤 Rat val", '𐂺 Vangkooi'))
     DICT_SORTGROUP = {"📷 Camera":"Camera", "🪤 Rat val":"Rat val",'𐂺 Vangkooi':'Vangkooi'}
     choice_opdracht = [DICT_SORTGROUP[item] for item in soortgroup]
     df_2 = df_2[df_2['soortgroup'].isin(choice_opdracht)]
