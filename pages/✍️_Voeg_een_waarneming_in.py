39de45bfd2911de98b4eba4abadbdedc1d33911b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-29 15:11:18 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -45,7 +45,7 @@ st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 # --- DIMENSIONS ---
 #innerWidth = streamlit_js_eval(js_expressions='screen.width',  want_output = True, key = 'width')
 #innerHeight = streamlit_js_eval(js_expressions='window.screen.height', want_output = True, key = 'height')
-OUTPUT_width = 300
+OUTPUT_width = 350
 OUTPUT_height = 550
 
     
