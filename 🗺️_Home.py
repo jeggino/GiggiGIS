0f33ed8b43680b64334aacafbd89f22e9d35f0fc
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-10 23:56:16 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -342,7 +342,6 @@ try:
     df_2["datum"] = pd.to_datetime(df_2["datum"]).dt.date
 
     st.sidebar.subheader("Filter op",divider=False)
-    #"YYYY-MM-DD"
     d = st.sidebar.slider("Datum", min_value=df_2.datum.min(),max_value=df_2.datum.max(),value=(df_2.datum.min(), df_2.datum.max()),format="DD-MM-YYYY")
     
     df_2 = df_2[(df_2['datum']>=d[0]) & (df_2['datum']<=d[1])]
