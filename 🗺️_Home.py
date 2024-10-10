5380dabc042a2ce15c44535944a33bf4e17b3971
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-11 00:13:26 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -372,7 +372,7 @@ try:
     for feature_group in functie_dictionary.keys():
         map.add_child(functie_dictionary[feature_group])
 
-    folium.TileLayer(tiles="CartoDB Positron",overlay=False,show=False).add_to(map)
+    folium.TileLayer(tiles="cartodbdark_matter",overlay=False,show=False).add_to(map)
     # folium.TileLayer('mapquestopen',overlay=False,show=False).add_to(map)
     # folium.TileLayer(tiles='stamentoner').add_to(map)
     # folium.TileLayer(tiles='cartodbpositron',overlay=False,show=False).add_to(map)
