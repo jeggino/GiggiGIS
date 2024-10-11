200b21a99b493389ae169cc82e6af50959fb8c53
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-11 11:07:12 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -359,7 +359,7 @@ try:
     #                  )
     df_2["icon_data"] = df_2.apply(lambda x: None if x["geometry_type"] in ["LineString","Polygon"] 
                                    else (icon_dictionary[x["soortgroup"]][x["sp"]][x["functie"]] if x["soortgroup"] in ['Vogels','Vleermuizen'] 
-                                         else icon_dictionary[x["soortgroup"]][x["functie"]]"), 
+                                         else icon_dictionary[x["soortgroup"]][x["functie"]]), 
                                    axis=1)
 
     map = folium.Map(tiles=None)
