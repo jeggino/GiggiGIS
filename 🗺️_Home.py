1290fffc2d1747bd7280599c6ada4b5ba051cc28
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 11:27:50 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -12,8 +12,6 @@ import random
 
 import ast
 
-from credentials import *
-
 
 # ---LAYOUT---
 st.set_page_config(
@@ -142,6 +140,80 @@ def logOut():
         del st.session_state.login
         # del st.session_state.project     
         st.rerun()
+
+# --- OPTIONS ---
+VLEERMUISKAST_OPTIONS = ["Bewoond","Onbewoond"]
+
+CAMERA_OPTIONS = ["Camera in het veld","Verwijderd, ratten gedetecteerd","Camera verwijderd, geen ratten gedetecteerd",
+                  "Waarneming rat doorgegeven, geen actie op ondernomen"]
+
+RAT_VAL_OPTIONS = ['Schietval in veld', 'Schietval in veld rat gedood','Schietval verwijderd, geen rat gedood','Schietval verwijderd, rat gedood']
+
+RAT_VANGKOOI_OPTIONS = ['vangkooi in veld','vangkooi in veld, rat gevangen','vangkooi verwijderd, rat gevangen','vangkooi verwijderd, geen rat gevangen']
+
+
+
+BAT_NAMES = ['Gewone dwergvleermuis','Ruige dwergvleermuis', 'Laatvlieger']
+
+BAT_BEHAVIOURS = ['foeragerend', 'uitvliegend','invliegend', 'overvliegend', 
+         'zwermend', 'sporen', 'balts', 'verkeersslachtoffer']
+
+BAT_FUNCTIE = ['geen / onbekend','zomerverblijfplaats','kraamverblijfplaats']
+
+BAT_VERBLIJF = ['geen / onbekend', 'dakgoot', 'spouwmuur', 'daklijst',
+       'kantpan', 'regenpijp', 'holte', 'raamkozijn', 'luik', 'scheur',
+       'schoorsteen', 'gevelbetimmering', 'nokpan', 'dakpan',
+       'vleermuiskast', 'openingen in dak', 'dakkapel', 'schors']
+
+BIRD_NAMES = ['Gierzwaluw','Huismus']
+
+BIRD_BEHAVIOURS = ['overvliegend',  'nest-indicerend gedrag', 'foeragerend', 'invliegend', 'uitvliegend',
+       'roepend vanuit gebouw', 'baltsend / zingend op gebouw',
+       'baltsend / zingend in vegetatie, struik of boom', 'sporen',
+        'copula']
+
+
+
+BIRD_FUNCTIE = ['geen / onbekend', 'nestlocatie', 'mogelijke nestlocatie']
+
+BIRD_VERBLIJF = ['geen / onbekend', 'dakgoot', 'kantpan', 'zonnepaneel', 'nokpan', 'nestkast',
+       'gevelbetimmering', 'openingen in dak', 'regenpijp',
+        'dakpan', 'spouwmuur', 'onder dakrand',
+       'raamkozijn', 'luik', 'schoorsteen', 'daklijst', 'dakkapel',
+       'in struweel / struiken', 'holte', 'op / bij nest in boom',
+       'scheur', 'vleermuiskast']
+
+VLEERMUISKAST_VERBLIJF = ["Op boom", "Op gebouw"]
+
+icon_dictionary = {'Vogels': {'Gierzwaluw': {'geen / onbekend': 'icons/swift.png',
+                                             'nestlocatie': 'icons/swift_nest.png',
+                                             'mogelijke nestlocatie': 'icons/swift_mogelijk_nest.png'},
+                              'Huismus': {'geen / onbekend': 'icons/sparrow.png',
+                                          'nestlocatie': 'icons/sparrow_nest.png',
+                                          'mogelijke nestlocatie': 'icons/sparrow_mogelijk_nest.png'}},
+                   'Vleermuizen': {'Gewone dwergvleermuis': {'geen / onbekend': 'icons/pippip_foraging.png',
+                                                             'zomerverblijfplaats': 'icons/pippip_zommer.png',
+                                                             'kraamverblijfplaats': 'icons/pippip_kraam.png'},
+                                   'Ruige dwergvleermuis': {'geen / onbekend': 'icons/ruige_foraging.png',
+                                                            'zomerverblijfplaats': 'icons/ruige_zommer.png',
+                                                            'kraamverblijfplaats': 'icons/ruige_kraam.png'},
+                                   'Laatvlieger': {'geen / onbekend': 'icons/laatflieger_foraging.png',
+                                                   'zomerverblijfplaats': 'icons/laatvlieger_zommer.png',
+                                                   'kraamverblijfplaats': 'icons/laatvlieger_kraam.png'}},
+                   'Vleermuiskast': {"Bewoond":"icons/bat_bow_full.jpg",
+                                     "Onbewoond":"icons/bat_box_empty.jpg"},
+                   'Camera': {'Camera in het veld': 'icons/camera-icon-orange.png',
+                              'Verwijderd, ratten gedetecteerd': 'icons/camera-icon-red.png',
+                              'Camera verwijderd, geen ratten gedetecteerd': 'icons/camera-icon-green.png',
+                              'Waarneming rat doorgegeven, geen actie op ondernomen': 'icons/rat_maybe_2.png'},
+                   'Rat val': {'Schietval in veld': 'icons/rat_trap_orange.png',
+                               'Schietval in veld rat gedood': 'icons/rat_trap_green.png',
+                               'Schietval verwijderd, geen rat gedood': 'icons/rat_trap_red.png',
+                               'Schietval verwijderd, rat gedood': 'icons/rat_trap_purple.png'},
+                   'Vangkooi': {'vangkooi in veld':'icons/rat_cage_veld_Nogevangen.png',
+                                'vangkooi in veld, rat gevangen':'icons/rat_cage_veld_gevangen.png',
+                                'vangkooi verwijderd, rat gevangen':'icons/rat_cage_noveld_gevangen.png',
+                                'vangkooi verwijderd, geen rat gevangen':'icons/rat_cage_noveld_Nogevangen.png'}}
 
 
 # --- APP ---
