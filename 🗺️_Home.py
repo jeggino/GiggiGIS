e8749a6f5713202fc9a0a551881cff8e09526838
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-23 11:32:42 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -235,9 +235,9 @@ st.logo(IMAGE,  link="https://www.elskenecologie.nl/#:~:text=Elsken%20Ecologie%2
 try:
     
     df_2 = df_point[df_point['project']=="Ratten Terschelling"]
-    # soortgroup = st.sidebar.multiselect("",("📷 Camera", "🪤 Rat val", '𐂺 Vangkooi'),("📷 Camera", "🪤 Rat val", '𐂺 Vangkooi'))
-    # DICT_SORTGROUP = {"📷 Camera":"Camera", "🪤 Rat val":"Rat val",'𐂺 Vangkooi':'Vangkooi'}
-    # choice_opdracht = [DICT_SORTGROUP[item] for item in soortgroup]
+    soortgroup =["📷 Camera", "🪤 Rat val", '𐂺 Vangkooi']
+    DICT_SORTGROUP = {"📷 Camera":"Camera", "🪤 Rat val":"Rat val",'𐂺 Vangkooi':'Vangkooi'}
+    choice_opdracht = [DICT_SORTGROUP[item] for item in soortgroup]
     # df_2 = df_2[df_2['soortgroup'].isin(choice_opdracht)]
     df_2["datum"] = pd.to_datetime(df_2["datum"]).dt.date
 
