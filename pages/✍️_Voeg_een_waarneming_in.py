191ea3e1f4b7b4e91bafcd2d66ea118789d1f7bc
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-25 19:50:18 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -191,7 +191,8 @@ def input_data(output,df_old):
     st.divider()
 
     # with st.form("my_form"):
-    submitted = st.button("**Gegevens opslaan**",use_container_width=True)
+    placeholder = st.empty()
+    submitted = placeholder.button("**Gegevens opslaan**",use_container_width=True)
     # submitted = st.form_submit_button("**Gegevens opslaan**",use_container_width=True)
     if submitted:           
         try:
@@ -216,12 +217,11 @@ def input_data(output,df_old):
                 st.stop()
 
             else:
-                placeholder = st.empty()
-                placeholder.warning("Weet u zeker dat u alle gegevens correct hebt ingevuld?")
+                placeholder.empty()
+                st.warning("Weet u zeker dat u alle gegevens correct hebt ingevuld?")
                 if st.button("🦇🪶🦇🪶 **Yeah!!** 🦇🪶🦇🪶",key="but_1",use_container_width=True):
                     insert_json(key,waarnemer,str(datum),str(datum_2),str(time),soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,coordinates,project,df_old)
                     st.success('Gegevens opgeslagen!', icon="✅") 
-                    st.rerun()
                   
         except:
             st.stop()
