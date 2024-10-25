585f69aa27e123394d5d029ccd32187166ea4924
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-25 19:43:10 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -218,11 +218,11 @@ def input_data(output,df_old):
             else:
                 # placeholder = st.empty()
                 # placeholder.warning("Weet u zeker dat u alle gegevens correct hebt ingevuld?")
-                if st.button("🦇🪶🦇🪶 **Yeah!!** 🦇🪶🦇🪶",key="but_1",use_container_width=True):
-                    insert_json(key,waarnemer,str(datum),str(datum_2),str(time),soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,coordinates,project,df_old)
-                    st.success('Gegevens opgeslagen!', icon="✅") 
-                    if st.session_state.project['auto_start']:
-                        st.switch_page("🗺️_Home.py")                  
+                # if st.button("🦇🪶🦇🪶 **Yeah!!** 🦇🪶🦇🪶",key="but_1",use_container_width=True):
+                insert_json(key,waarnemer,str(datum),str(datum_2),str(time),soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,coordinates,project,df_old)
+                st.success('Gegevens opgeslagen!', icon="✅") 
+                if st.session_state.project['auto_start']:
+                    st.switch_page("🗺️_Home.py")                  
         except:
             st.error('error')
             st.stop()
