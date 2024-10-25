f8b8db6e3717e47d81d69fed9e98efcd2e11a985
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-25 20:29:57 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -56,7 +56,7 @@ OUTPUT_width = '95%'
 OUTPUT_height = 550
 
 #---DATASET---
-ttl = 0
+ttl = '10m'
 ttl_references = '10m'
 conn = st.connection("gsheets", type=GSheetsConnection)
 df_point = conn.read(ttl=ttl,worksheet="ratten-terschelling")
