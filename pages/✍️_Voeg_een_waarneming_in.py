9387876f029ca1a33bb8264ecd3272654529b084
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-25 19:10:37 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -222,8 +222,8 @@ def input_data(output):
                     if st.button("🦇🪶🦇🪶 **Yeah!!** 🦇🪶🦇🪶"):
                         insert_json(key,waarnemer,str(datum),str(datum_2),str(time),soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,coordinates,project)
                         st.success('Gegevens opgeslagen!', icon="✅") 
-                        if st.session_state.project['auto_start']:
-                            st.switch_page("🗺️_Home.py")                  
+                        # if st.session_state.project['auto_start']:
+                        #     st.switch_page("🗺️_Home.py")                  
             except:
                 st.stop()
 
