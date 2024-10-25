ba9a4dbf98edea6653862a0573d5b4b02530baa4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-25 13:05:28 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -240,94 +240,94 @@ with st.sidebar:
 IMAGE = "image/logo.png"
 st.logo(IMAGE,  link="https://www.elskenecologie.nl/#:~:text=Elsken%20Ecologie%20is%20het%20onafhankelijke%20ecologisch%20advies-%20en", icon_image=None)
 
-try:
+# try:
     
-    df_2 = df_point
-    df_2["datum"] = pd.to_datetime(df_2["datum"]).dt.date
-        
-    st.sidebar.subheader("Filter op",divider=False)
-    d = st.sidebar.slider("Datum", min_value=df_2.datum.min(),max_value=df_2.datum.max(),value=(df_2.datum.min(), df_2.datum.max()),format="DD-MM-YYYY")
-  
-    df_2 = df_2[(df_2['datum']>=d[0]) & (df_2['datum']<=d[1])]
-
-    st.sidebar.divider()
-
-    df_2["icon_data"] = df_2.apply(lambda x: icon_dictionary[x["soortgroup"]][x["sp"]][x["functie"]] if x["soortgroup"] in ['Vogels','Vleermuizen'] 
-                                   else icon_dictionary[x["soortgroup"]][x["functie"]], 
-                                   axis=1
-                     )
+df_2 = df_point
+df_2["datum"] = pd.to_datetime(df_2["datum"]).dt.date
     
-    map = folium.Map(location=(df_2["lat"].mean(), df_2["lng"].mean()),zoom_start=11,tiles=None)
-    LocateControl(auto_start=True).add_to(map)
-    Fullscreen().add_to(map)
+st.sidebar.subheader("Filter op",divider=False)
+d = st.sidebar.slider("Datum", min_value=df_2.datum.min(),max_value=df_2.datum.max(),value=(df_2.datum.min(), df_2.datum.max()),format="DD-MM-YYYY")
+
+df_2 = df_2[(df_2['datum']>=d[0]) & (df_2['datum']<=d[1])]
+
+st.sidebar.divider()
+
+df_2["icon_data"] = df_2.apply(lambda x: icon_dictionary[x["soortgroup"]][x["sp"]][x["functie"]] if x["soortgroup"] in ['Vogels','Vleermuizen'] 
+                               else icon_dictionary[x["soortgroup"]][x["functie"]], 
+                               axis=1
+                 )
+
+map = folium.Map(location=(df_2["lat"].mean(), df_2["lng"].mean()),zoom_start=11,tiles=None)
+LocateControl(auto_start=True).add_to(map)
+Fullscreen().add_to(map)
+
+functie_dictionary = {}
+functie_len = df_2['functie'].unique()
+
+for functie in functie_len:
+    functie_dictionary[functie] = folium.FeatureGroup(name=functie)     
+
+for feature_group in functie_dictionary.keys():
+    map.add_child(functie_dictionary[feature_group])
+
+folium.TileLayer('OpenStreetMap',overlay=False,show=True,name="Stratenkaart").add_to(map)
+folium.TileLayer(tiles="CartoDB Positron",overlay=False,show=False,name="Witte kaart").add_to(map)
+folium.TileLayer(tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr='Google_map',overlay=False,show=False,name="Satellietkaart").add_to(map)
+folium.LayerControl().add_to(map)    
+
+groups={}
+
+for group in choice_opdracht:
+    groups[group] = list(df_2[df_2.soortgroup==group]["functie"].unique())
+    feature = [functie_dictionary[i] for i in groups[group]]
+
+    key_list = list(DICT_SORTGROUP.keys())
+    val_list = list(DICT_SORTGROUP.values())
+    position = val_list.index(group)
     
-    functie_dictionary = {}
-    functie_len = df_2['functie'].unique()
+    groups[group] = feature
+
+groups = dict(zip(soortgroup, list(groups.values())))
+GroupedLayerControl(
+    groups=groups,
+    exclusive_groups=False,
+    collapsed=True,
+).add_to(map)
+
+for i in range(len(df_2)):
+
+    if df_2.iloc[i]['geometry_type'] == "Point":
+
+        if (df_2.iloc[i]['sp']=="Huismus") & (df_2.iloc[i]['functie'] in ["mogelijke nestlocatie","nestlocatie"]):
+            ICON_SIZE_2 = ICON_SIZE_huismus
+
+        elif df_2.iloc[i]['functie'] == "Waarneming rat doorgegeven, geen actie op ondernomen":
+            ICON_SIZE_2 = ICON_SIZE_rat_maybe
+
+        else:
+            ICON_SIZE_2 = ICON_SIZE
+            
+
+        html = popup_html(i)
+        popup = folium.Popup(folium.Html(html, script=True), max_width=300)
+        fouctie_loop = functie_dictionary[df_2.iloc[i]['functie']]
+
+        folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
+                      popup=popup,
+                      icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=ICON_SIZE_2)
+                     ).add_to(fouctie_loop)
+            
+
+    elif df_2.iloc[i]['geometry_type'] == "LineString":
+
+        folium.PolyLine(df_2.iloc[i]['coordinates']).add_to(fg)
+
     
-    for functie in functie_len:
-        functie_dictionary[functie] = folium.FeatureGroup(name=functie)     
-    
-    for feature_group in functie_dictionary.keys():
-        map.add_child(functie_dictionary[feature_group])
+col_1,col_2,col_3 = st.columns([1,11,1],gap="small") 
 
-    folium.TileLayer('OpenStreetMap',overlay=False,show=True,name="Stratenkaart").add_to(map)
-    folium.TileLayer(tiles="CartoDB Positron",overlay=False,show=False,name="Witte kaart").add_to(map)
-    folium.TileLayer(tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr='Google_map',overlay=False,show=False,name="Satellietkaart").add_to(map)
-    folium.LayerControl().add_to(map)    
+with col_2:
+    output_2 = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,feature_group_to_add=list(functie_dictionary.values()))
 
-    groups={}
-
-    for group in choice_opdracht:
-        groups[group] = list(df_2[df_2.soortgroup==group]["functie"].unique())
-        feature = [functie_dictionary[i] for i in groups[group]]
-    
-        key_list = list(DICT_SORTGROUP.keys())
-        val_list = list(DICT_SORTGROUP.values())
-        position = val_list.index(group)
-        
-        groups[group] = feature
-    
-    groups = dict(zip(soortgroup, list(groups.values())))
-    GroupedLayerControl(
-        groups=groups,
-        exclusive_groups=False,
-        collapsed=True,
-    ).add_to(map)
-
-    for i in range(len(df_2)):
-
-        if df_2.iloc[i]['geometry_type'] == "Point":
-
-            if (df_2.iloc[i]['sp']=="Huismus") & (df_2.iloc[i]['functie'] in ["mogelijke nestlocatie","nestlocatie"]):
-                ICON_SIZE_2 = ICON_SIZE_huismus
-
-            elif df_2.iloc[i]['functie'] == "Waarneming rat doorgegeven, geen actie op ondernomen":
-                ICON_SIZE_2 = ICON_SIZE_rat_maybe
-
-            else:
-                ICON_SIZE_2 = ICON_SIZE
-                
-
-            html = popup_html(i)
-            popup = folium.Popup(folium.Html(html, script=True), max_width=300)
-            fouctie_loop = functie_dictionary[df_2.iloc[i]['functie']]
-    
-            folium.Marker([df_2.iloc[i]['lat'], df_2.iloc[i]['lng']],
-                          popup=popup,
-                          icon=folium.features.CustomIcon(df_2.iloc[i]["icon_data"], icon_size=ICON_SIZE_2)
-                         ).add_to(fouctie_loop)
-                
-
-        elif df_2.iloc[i]['geometry_type'] == "LineString":
-
-            folium.PolyLine(df_2.iloc[i]['coordinates']).add_to(fg)
-
-        
-    col_1,col_2,col_3 = st.columns([1,11,1],gap="small") 
-
-    with col_2:
-        output_2 = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,feature_group_to_add=list(functie_dictionary.values()))
-
-except:
-    st.image("https://media.istockphoto.com/photos/open-empty-cardboard-box-on-a-white-background-picture-id172167710?k=6&m=172167710&s=612x612&w=0&h=Z4fueCweh9q-X_VBRAPCYSalyaAnXG3ioErb8oJSVek=")
-    st.stop()
+# except:
+#     st.image("https://media.istockphoto.com/photos/open-empty-cardboard-box-on-a-white-background-picture-id172167710?k=6&m=172167710&s=612x612&w=0&h=Z4fueCweh9q-X_VBRAPCYSalyaAnXG3ioErb8oJSVek=")
+#     st.stop()
