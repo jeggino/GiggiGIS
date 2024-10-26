f3645df796c1cd174c578311d246ecd039e80962
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-26 12:31:05 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -168,7 +168,6 @@ def input_data(output,df_old):
 
         else:
             placeholder.success('Gegevens opgeslagen!', icon="✅",)
-            output_map["features"][:] = []
             insert_json(key,waarnemer,str(datum),str(time),soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,coordinates,project,df_old)
             # st.rerun()
 
