8fb2d202878045d0a6d2d2672c6b7362ff0553bb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-26 12:32:23 +0200

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -18,9 +18,9 @@ from credentials import *
 
 # ---LAYOUT---
 st.set_page_config(
-    page_title="🦇🪶",
+    page_title="🦇🪶 SMPs",
     initial_sidebar_state="collapsed",
-    page_icon="🦇🪶",
+    page_icon="",
     layout="wide",
     
 )
