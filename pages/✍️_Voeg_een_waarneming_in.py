9ddadeecc9b33efc58e4d5b3b05f7045c16973a3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-26 11:02:13 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -19,7 +19,7 @@ from credentials import *
 st.set_page_config(
     page_title="🦇🪶",
     initial_sidebar_state="collapsed",
-    page_icon=" ",
+    page_icon="SMPs ",
     layout="wide",
     
 )
