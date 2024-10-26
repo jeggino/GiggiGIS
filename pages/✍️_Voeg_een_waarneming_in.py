e5bd5e6daa3a9e668b60cb7435009514b694616b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-26 11:55:00 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -57,7 +57,7 @@ def insert_json(key,waarnemer,datum,time,soortgroup,aantal,sp,gedrag,functie,ver
     df_new = pd.DataFrame(data)
     df_updated = pd.concat([df_old,df_new],ignore_index=True)
     
-    return conn.update(worksheet="df_observations",data=df_updated)
+    return conn.update(worksheet="df_observations",data=df_updated),output_map["features"][0].clear()
   
 def map(auto_start):
     
@@ -209,7 +209,7 @@ try:
     try:
         if len(output_map["features"]) != 0:
             input_data(output_map,df_old)
-            output_map["features"][0].clear()
+            
             
             
     except:
