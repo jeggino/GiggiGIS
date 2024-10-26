7aa62cf4c79792842b6e55314e7f98582e274e1d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-26 10:53:18 +0200

    Update ✍️_Voeg_een_waarneming_in.py

--- pages/✍️_Voeg_een_waarneming_in.py
+++ pages/✍️_Voeg_een_waarneming_in.py
@@ -170,8 +170,9 @@ def input_data(output,df_old):
                 placeholder.empty()
                 st.warning("Weet u zeker dat u alle gegevens correct hebt ingevuld?")
                 if st.button("🦇🪶🦇🪶 **Yeah!!** 🦇🪶🦇🪶",key="but_1",use_container_width=True):
+                    st.success('Gegevens opgeslagen!', icon="✅")
                     insert_json(key,waarnemer,str(datum),str(time),soortgroup,aantal,sp,gedrag,functie,verblijf,geometry_type,lat,lng,opmerking,coordinates,project,df_old)
-                    st.success('Gegevens opgeslagen!', icon="✅") 
+                     
                   
         except:
             st.error("!!!!!!!!!!!!!!!!!!!!")
