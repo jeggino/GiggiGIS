e16e29b466853f72ca47ef19fd22027845bbf9f1
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-28 08:58:09 +0100

    Update 3📊_ Statistik.py

--- pages/3📊_ Statistik.py
+++ pages/3📊_ Statistik.py
@@ -26,5 +26,6 @@ df_point = df_point[(df_point['project']==project)&(df_point['soortgroup']==opdr
 df_point
 option_1 = st.selectbox("Option 1",('zomerverblijfplaats','kraamverblijfplaats','paarverblijfplaats', 'winterverblijfplaats'))
 option_1
+df_point
 df_point_option_1 = df_point.groupby(['gebied',option_1],as_index=False).size()
 df_point_option_1
