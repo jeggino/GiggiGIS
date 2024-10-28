ee7dfb94a7525a1116415307cbcb06cf393a8d0e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-28 10:38:42 +0100

    Update 3📊_ Statistik.py

--- pages/3📊_ Statistik.py
+++ pages/3📊_ Statistik.py
@@ -95,8 +95,8 @@ col_2.pydeck_chart(pydeck_obj=r,use_container_width=True, width=None, height=400
 
 "---"
 col_3,col_4 = st.columns([1,5])
-option_2 = col_3.selectbox("Option 2",gdf_areas['gebied'].unique())
-df_dagverslag_option_2 = df_dagverslag[df_dagverslag['Wijk']==option_2]
+option_2 = col_3.selectbox("Option 2",gdf_areas['Wijk'].unique())
+df_dagverslag_option_2 = df_dagverslag[df_dagverslag['gebied']==option_2]
 
 chart = alt.Chart(df_dagverslag_option_2).mark_point(size=60).encode(
     alt.X('datum:T',axis=alt.Axis(grid=False,domain=True,ticks=False,),title=None, 
