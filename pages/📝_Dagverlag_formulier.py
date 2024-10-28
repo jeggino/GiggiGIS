a01bcb44725c24509e9fbe302cb9afcf17948fd7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-28 04:51:40 +0100

    Update 📝_Dagverlag_formulier.py

--- pages/📝_Dagverlag_formulier.py
+++ pages/📝_Dagverlag_formulier.py
@@ -34,7 +34,6 @@ reduce_header_height_style = """
 
 st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
-st.write('NOT YET!!')
 # --- FUNCTIONS ---
 def insert_dagverslag(key,waarnemer,opdracht,gebied_id,bemonsteringsmoment,datum,start_time,eind_time,extra_velfwerker,temperatuur,bewolking,neerslag,windkrcht,windrichting,opmerking,df_old):
     
@@ -69,29 +68,30 @@ try:
 except:
     pass
 
-bemonsteringsmoment = st.selectbox('Bemonsteringsmoment',('Kraamverblijf','Winterverblijf','Paarverblijf'))
-datum = st.date_input("Datum","today")       
-two_hours_from_now = datetime.now() + timedelta(hours=1)
-four_hours_from_now = datetime.now() + timedelta(hours=3)
-start_time = st.time_input("Start tijd", two_hours_from_now)
-eind_time = st.time_input("Eind tijd", four_hours_from_now)
-
-extra_velfwerker_list = df_projects.set_index('project').loc[project,"user"].split(',')
-extra_velfwerker_list.remove(waarnemer)
-extra_velfwerker = st.multiselect("Extra velfwerker",extra_velfwerker_list)
-
-temperatuur = st.number_input("Temperatuur",key='temperatuur', min_value=0)
-bewolking = st.selectbox("Bewolking",("Onbewolkt (<10%)", "Halfbewolkt (10-80%)", "Bewolkt (>80%)"))
-neerslag = st.selectbox("Neerslag",("Droog", "Nevel/mist", "Motregen", "Regen","Zware regen","Sneeuw"))
-windkrcht = st.number_input("windkrcht",key='windkrcht', min_value=1)
-windrichting = st.selectbox("Windrichting",("Noord", "Noordoost", "Oost", "Zuidoost","Zuid","Zuidwest","West","Noordwest"))
-
-if gebied_id == None:
-    st.markdown("Vergeet a.u.b. niet in de opmerking te schrijven welke soort je hebt gevonden, de dichtstbijzijnde locaties en het doel van het onderzoek.")
-    
-opmerking = st.text_input("", placeholder="Vul hier een opmerking in ...")
-
 with st.form("my_form", clear_on_submit=True,border=False):
+    bemonsteringsmoment = st.selectbox('Bemonsteringsmoment',('Kraamverblijf','Winterverblijf','Paarverblijf'))
+    datum = st.date_input("Datum","today")       
+    two_hours_from_now = datetime.now() + timedelta(hours=1)
+    four_hours_from_now = datetime.now() + timedelta(hours=3)
+    start_time = st.time_input("Start tijd", two_hours_from_now)
+    eind_time = st.time_input("Eind tijd", four_hours_from_now)
+    
+    extra_velfwerker_list = df_projects.set_index('project').loc[project,"user"].split(',')
+    extra_velfwerker_list.remove(waarnemer)
+    extra_velfwerker = st.multiselect("Extra velfwerker",extra_velfwerker_list)
+    
+    temperatuur = st.number_input("Temperatuur",key='temperatuur', min_value=0)
+    bewolking = st.selectbox("Bewolking",("Onbewolkt (<10%)", "Halfbewolkt (10-80%)", "Bewolkt (>80%)"))
+    neerslag = st.selectbox("Neerslag",("Droog", "Nevel/mist", "Motregen", "Regen","Zware regen","Sneeuw"))
+    windkrcht = st.number_input("windkrcht",key='windkrcht', min_value=1)
+    windrichting = st.selectbox("Windrichting",("Noord", "Noordoost", "Oost", "Zuidoost","Zuid","Zuidwest","West","Noordwest"))
+    
+    if gebied_id == None:
+        st.markdown("Vergeet a.u.b. niet in de opmerking te schrijven welke soort je hebt gevonden, de dichtstbijzijnde locaties en het doel van het onderzoek.")
+        
+    opmerking = st.text_input("", placeholder="Vul hier een opmerking in ...")
+    
+    
     if st.form_submit_button("**Gegevens opslaan**",use_container_width=True):
         insert_dagverslag(key,waarnemer,opdracht,gebied_id,bemonsteringsmoment,datum,start_time,eind_time,extra_velfwerker,temperatuur,bewolking,neerslag,windkrcht,windrichting,opmerking,df_old)
     
