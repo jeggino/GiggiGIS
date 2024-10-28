3656d63a28c8b13754d40d23c48279c055abd1d4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-28 08:20:23 +0100

    Update 3📊_ Statistik.py

--- pages/3📊_ Statistik.py
+++ pages/3📊_ Statistik.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import geopandas as gpd
+
+import pydeck as pdk
+import datetime
+from datetime import datetime, timedelta, date
+import random
 
 
+st.session_state.project.gdf
 st.write("Not yet")
