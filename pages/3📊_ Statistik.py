5c0550af6f85e56d23516ae128598ee471ae583d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-28 12:38:03 +0100

    Update 3📊_ Statistik.py

--- pages/3📊_ Statistik.py
+++ pages/3📊_ Statistik.py
@@ -129,7 +129,7 @@ with tab2:
             [0, 190, 0, 190],
             [0, 255, 0, 255],
         ],
-        get_position=['lat','lng'],
+        get_position=['lng','lat'],
     )
 
     r = pdk.Deck(layers=[layer], initial_view_state=INITIAL_VIEW_STATE)
