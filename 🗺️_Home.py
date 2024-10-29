302564c38e0bbe21d4b3c90ce7bd9370a6437735
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-29 15:28:29 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -449,7 +449,7 @@ for i in range(len(df_2)):
 
     if df_2.iloc[i]['geometry_type'] == "Point":
 
-        if (df_2.iloc[i]['sp']=="Huismus")):
+        if (df_2.iloc[i]['sp']=="Huismus"):
             ICON_SIZE_2 = ICON_SIZE_huismus
 
 
