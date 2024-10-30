fd132878813cdce88c2226121e145e5efc81bf02
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-30 13:58:23 +0100

    Update 3📊_ Statistik.py

--- pages/3📊_ Statistik.py
+++ pages/3📊_ Statistik.py
@@ -143,7 +143,7 @@ try:
     
         r_2 = pdk.Deck(layers=[layer], initial_view_state=INITIAL_VIEW_STATE,
                        map_provider='mapbox', 
-                       map_style="mapbox://styles/jeggino/cm2vtvb2l000w01qz9wet0mv9",)
+                       map_style="mapbox://styles/jeggino/clidbo1ju002m01pa9agd14zd",)
         
         col_4.pydeck_chart(pydeck_obj=r_2,use_container_width=True, width=None, height=400, selection_mode="single-object", on_select="ignore", key=None)
       
