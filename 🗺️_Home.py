e194d3568e995250aaa0dd21ea786a1f5a708dcb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-30 18:49:39 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -74,7 +74,7 @@ ICON_SIZE_huismus = (28,28)
 ICON_SIZE_BAT_EXTRA = (60,25)
 ICON_SIZE_RUIGE = (50,50)
 ICON_SIZE_BIRD = (70,50)
-ICON_SIZE_Huiszwaluw = (90,60)
+ICON_SIZE_Huiszwaluw = (90,45)
 
 
 # --- FUNCTIONS ---
