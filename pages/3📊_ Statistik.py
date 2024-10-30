f0f10e72c1045f71761a677673ace7be7f926f33
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-30 11:20:49 +0100

    Update 3📊_ Statistik.py

--- pages/3📊_ Statistik.py
+++ pages/3📊_ Statistik.py
@@ -54,37 +54,126 @@ IMAGE = "image/logo.png"
 IMAGE_2 ="image/menu.jpg"
 st.logo(IMAGE,  link=None, icon_image=IMAGE_2)
 
-project = st.session_state["project"]['project_name']
-opdracht = st.session_state["project"]['opdracht']
-gdf_areas = gpd.read_file(f"geometries/{project}.geojson")
-gdf_areas.geometry = gdf_areas.geometry.apply(lambda x: Polygon(x.coords)) 
-df_point = df_point[(df_point['project']==project)&(df_point['soortgroup']==opdracht)&(df_point['geometry_type']=="Point")].reset_index(drop=True)
-
-if opdracht == 'Vogels':
-    functie = BIRD_FUNCTIE
-    option_3 = st.selectbox("Selecteer een soort",BIRD_NAMES ,label_visibility="visible")
-    df_point = df_point[df_point['sp']==option_3]
-elif opdracht == 'Vleermuizen':
-    functie =BAT_FUNCTIE 
- 
-option_1 = st.selectbox("Selecteer een functie",functie,
-                        label_visibility="visible",help="Selecteer een specifieke functie om de gebieden met de hoogste concentratie te identificeren en hun verspreiding beter te begrijpen.")
-
-tab1, tab2 = st.tabs(["PolygonLayer", "Screengrid"])
-with tab1:
-    col_1,col_2 = st.columns([1,2])
-    df_point_option_1 = df_point[df_point['functie']==option_1]
-    df_point_option_2 = df_point_option_1.groupby(['gebied'],as_index=False).size()
-    df_merge_option_1 = gdf_areas.rename(columns={'Wijk':'gebied'}).merge(df_point_option_2, on='gebied',how='left').fillna(0)
+try:
+    project = st.session_state["project"]['project_name']
+    opdracht = st.session_state["project"]['opdracht']
+    gdf_areas = gpd.read_file(f"geometries/{project}.geojson")
+    gdf_areas.geometry = gdf_areas.geometry.apply(lambda x: Polygon(x.coords)) 
+    df_point = df_point[(df_point['project']==project)&(df_point['soortgroup']==opdracht)&(df_point['geometry_type']=="Point")].reset_index(drop=True)
     
-    chart_1 = alt.Chart(df_merge_option_1).mark_bar().encode(x=alt.X('size:Q',axis=alt.Axis(grid=False,domain=True,ticks=False),title=None, 
-                                                                     scale=alt.Scale(domain=[0,df_merge_option_1['size'].max()+2])),
-                                                             y=alt.Y('gebied:N',
-                                                                   axis=alt.Axis(grid=False,domain=False,ticks=True,title=None),
-                                                                   sort=alt.EncodingSortField(field="size",  order='descending'),
-                                                                   title="")
-                                                            ).properties(
-        height=400,
+    if opdracht == 'Vogels':
+        functie = BIRD_FUNCTIE
+        option_3 = st.selectbox("Selecteer een soort",BIRD_NAMES ,label_visibility="visible")
+        df_point = df_point[df_point['sp']==option_3]
+    elif opdracht == 'Vleermuizen':
+        functie =BAT_FUNCTIE 
+     
+    option_1 = st.selectbox("Selecteer een functie",functie,
+                            label_visibility="visible",help="Selecteer een specifieke functie om de gebieden met de hoogste concentratie te identificeren en hun verspreiding beter te begrijpen.")
+    
+    tab1, tab2 = st.tabs(["PolygonLayer", "Screengrid"])
+    with tab1:
+        col_1,col_2 = st.columns([1,2])
+        df_point_option_1 = df_point[df_point['functie']==option_1]
+        df_point_option_2 = df_point_option_1.groupby(['gebied'],as_index=False).size()
+        df_merge_option_1 = gdf_areas.rename(columns={'Wijk':'gebied'}).merge(df_point_option_2, on='gebied',how='left').fillna(0)
+        
+        chart_1 = alt.Chart(df_merge_option_1).mark_bar().encode(x=alt.X('size:Q',axis=alt.Axis(grid=False,domain=True,ticks=False),title=None, 
+                                                                         scale=alt.Scale(domain=[0,df_merge_option_1['size'].max()+2])),
+                                                                 y=alt.Y('gebied:N',
+                                                                       axis=alt.Axis(grid=False,domain=False,ticks=True,title=None),
+                                                                       sort=alt.EncodingSortField(field="size",  order='descending'),
+                                                                       title="")
+                                                                ).properties(
+            height=400,
+            title=alt.Title(
+                text="",
+                subtitle="",
+                anchor='start'
+            )
+        ).configure_view(stroke=None)
+        
+        col_1.altair_chart(chart_1, theme=None, use_container_width=True)
+        
+        INITIAL_VIEW_STATE = pdk.ViewState(latitude=gdf_areas.dissolve().centroid.y[0], longitude=gdf_areas.dissolve().centroid.x[0], zoom=11, max_zoom=16, pitch=45, bearing=0)
+        
+        geojson = pdk.Layer(
+            "GeoJsonLayer",
+            df_merge_option_1,
+            opacity=0.8,
+            stroked=False,
+            filled=True,
+            pickable=True,
+            extruded=True,
+            wireframe=True,
+            get_elevation="size * 200",
+            get_fill_color="[255, 255, size * 100]",
+            get_line_color=[255, 255, 255],
+        )
+        
+        tooltip = {
+            "html": "<b>{gebied}</b> <br /><b>Aantal: {size}</b>",
+            "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+        }
+        
+        r = pdk.Deck(layers=[geojson], initial_view_state=INITIAL_VIEW_STATE,tooltip=tooltip)
+        
+        col_2.pydeck_chart(pydeck_obj=r,use_container_width=True, width=None, height=400, selection_mode="single-object", on_select="ignore", key=None)
+    
+    with tab2:
+        col_3,col_4 = st.columns([1,4])
+        
+        col_3.metric(label="Totaal", value=len(df_point_option_1))
+        
+        INITIAL_VIEW_STATE = pdk.ViewState(latitude=gdf_areas.dissolve().centroid.y[0], longitude=gdf_areas.dissolve().centroid.x[0], zoom=11, max_zoom=16, pitch=45, bearing=0)
+        
+        layer = pdk.Layer(
+            "ScreenGridLayer",
+            df_point_option_1,
+            pickable=True,
+            opacity=0.8,
+            cell_size_pixels=50,
+            get_position=['lng','lat'],
+        )
+    
+        r_2 = pdk.Deck(layers=[layer], initial_view_state=INITIAL_VIEW_STATE)
+        
+        col_4.pydeck_chart(pydeck_obj=r_2,use_container_width=True, width=None, height=400, selection_mode="single-object", on_select="ignore", key=None)
+      
+    "---"
+    
+    option_2 = st.selectbox("Selecteer een gebied",gdf_areas['Wijk'].unique(),
+                            label_visibility="visible",help="Selecteer een gebied om het bemonsteringsproces te beoordelen. Door op de punten te klikken, kunt u gedetailleerde informatie over de individuele enquête ophalen.")
+    df_dagverslag_option_2 = df_dagverslag[df_dagverslag['gebied_id']==option_2]
+    df_dagverslag_option_2['datum'] = pd.to_datetime(df_dagverslag_option_2['datum'])
+    df_dagverslag_option_2['year'] = df_dagverslag_option_2['datum'].dt.year
+    
+    year_min = df_dagverslag_option_2['year'].min()
+    year_max = df_dagverslag_option_2['year'].max() + 1
+    
+    chart_2 = alt.Chart(df_dagverslag_option_2).mark_point(size=60).encode(
+        alt.X('datum:T',axis=alt.Axis(grid=False,domain=True,ticks=False,),title=None, 
+              scale=alt.Scale(domain=[str(year_min),str(year_max)])),
+        alt.Y('gebied_id:N',
+              axis=alt.Axis(grid=False,domain=False,ticks=True,title=None),
+              title=""),
+        stroke=alt.Color('doel'),
+        fill=alt.Color('doel',legend=alt.Legend(orient="bottom",direction='vertical',titleAnchor='middle')).title("Doel"),
+        tooltip=[alt.Tooltip("waarnemer:N",title = "Waarnemer"),
+                 alt.Tooltip("extra_velfwerker:N",title ="Extra veldwerkers"),
+                 alt.Tooltip("doel:N",title ="Doel"),
+                 alt.Tooltip("datum:T",title ="Datum"),
+                 alt.Tooltip("start_time:N",title ="Begin tijd"),
+                 alt.Tooltip("eind_time:N",title ="Eind tijd"),
+                 alt.Tooltip("temperatuur:N",title ="Temperatuur"),
+                 alt.Tooltip("bewolking:N",title ="Bewolking"),
+                 alt.Tooltip("neerslag:N",title ="Neerslag"),
+                 alt.Tooltip("windkrcht:N",title ="Windkrcht"),
+                 alt.Tooltip("windrichting:N",title ="Windrichting"),
+                 alt.Tooltip("opmerking:N",title ="Opmerking"),
+                ],
+    ).properties(
+        height=230,
         title=alt.Title(
             text="",
             subtitle="",
@@ -92,92 +181,7 @@ with tab1:
         )
     ).configure_view(stroke=None)
     
-    col_1.altair_chart(chart_1, theme=None, use_container_width=True)
-    
-    INITIAL_VIEW_STATE = pdk.ViewState(latitude=gdf_areas.dissolve().centroid.y[0], longitude=gdf_areas.dissolve().centroid.x[0], zoom=11, max_zoom=16, pitch=45, bearing=0)
-    
-    geojson = pdk.Layer(
-        "GeoJsonLayer",
-        df_merge_option_1,
-        opacity=0.8,
-        stroked=False,
-        filled=True,
-        pickable=True,
-        extruded=True,
-        wireframe=True,
-        get_elevation="size * 200",
-        get_fill_color="[255, 255, size * 100]",
-        get_line_color=[255, 255, 255],
-    )
-    
-    tooltip = {
-        "html": "<b>{gebied}</b> <br /><b>Aantal: {size}</b>",
-        "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
-    }
-    
-    r = pdk.Deck(layers=[geojson], initial_view_state=INITIAL_VIEW_STATE,tooltip=tooltip)
-    
-    col_2.pydeck_chart(pydeck_obj=r,use_container_width=True, width=None, height=400, selection_mode="single-object", on_select="ignore", key=None)
+    st.altair_chart(chart_2, theme=None, use_container_width=True)
 
-with tab2:
-    col_3,col_4 = st.columns([1,4])
-    
-    col_3.metric(label="Totaal", value=len(df_point_option_1))
-    
-    INITIAL_VIEW_STATE = pdk.ViewState(latitude=gdf_areas.dissolve().centroid.y[0], longitude=gdf_areas.dissolve().centroid.x[0], zoom=11, max_zoom=16, pitch=45, bearing=0)
-    
-    layer = pdk.Layer(
-        "ScreenGridLayer",
-        df_point_option_1,
-        pickable=True,
-        opacity=0.8,
-        cell_size_pixels=50,
-        get_position=['lng','lat'],
-    )
-
-    r_2 = pdk.Deck(layers=[layer], initial_view_state=INITIAL_VIEW_STATE)
-    
-    col_4.pydeck_chart(pydeck_obj=r_2,use_container_width=True, width=None, height=400, selection_mode="single-object", on_select="ignore", key=None)
-  
-"---"
-
-option_2 = st.selectbox("Selecteer een gebied",gdf_areas['Wijk'].unique(),
-                        label_visibility="visible",help="Selecteer een gebied om het bemonsteringsproces te beoordelen. Door op de punten te klikken, kunt u gedetailleerde informatie over de individuele enquête ophalen.")
-df_dagverslag_option_2 = df_dagverslag[df_dagverslag['gebied_id']==option_2]
-df_dagverslag_option_2['datum'] = pd.to_datetime(df_dagverslag_option_2['datum'])
-df_dagverslag_option_2['year'] = df_dagverslag_option_2['datum'].dt.year
-
-year_min = df_dagverslag_option_2['year'].min()
-year_max = df_dagverslag_option_2['year'].max() + 1
-
-chart_2 = alt.Chart(df_dagverslag_option_2).mark_point(size=60).encode(
-    alt.X('datum:T',axis=alt.Axis(grid=False,domain=True,ticks=False,),title=None, 
-          scale=alt.Scale(domain=[str(year_min),str(year_max)])),
-    alt.Y('gebied_id:N',
-          axis=alt.Axis(grid=False,domain=False,ticks=True,title=None),
-          title=""),
-    stroke=alt.Color('doel'),
-    fill=alt.Color('doel',legend=alt.Legend(orient="bottom",direction='vertical',titleAnchor='middle')).title("Doel"),
-    tooltip=[alt.Tooltip("waarnemer:N",title = "Waarnemer"),
-             alt.Tooltip("extra_velfwerker:N",title ="Extra veldwerkers"),
-             alt.Tooltip("doel:N",title ="Doel"),
-             alt.Tooltip("datum:T",title ="Datum"),
-             alt.Tooltip("start_time:N",title ="Begin tijd"),
-             alt.Tooltip("eind_time:N",title ="Eind tijd"),
-             alt.Tooltip("temperatuur:N",title ="Temperatuur"),
-             alt.Tooltip("bewolking:N",title ="Bewolking"),
-             alt.Tooltip("neerslag:N",title ="Neerslag"),
-             alt.Tooltip("windkrcht:N",title ="Windkrcht"),
-             alt.Tooltip("windrichting:N",title ="Windrichting"),
-             alt.Tooltip("opmerking:N",title ="Opmerking"),
-            ],
-).properties(
-    height=230,
-    title=alt.Title(
-        text="",
-        subtitle="",
-        anchor='start'
-    )
-).configure_view(stroke=None)
-
-st.altair_chart(chart_2, theme=None, use_container_width=True)
+except:
+    st.switch_page("🗺️_Home.py")
