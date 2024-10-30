716ed786bcb99a6e90d0c52bbcb097cdeeda06e3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-30 17:01:49 +0100

    Update 3📊_ Statistik.py

--- pages/3📊_ Statistik.py
+++ pages/3📊_ Statistik.py
@@ -197,7 +197,7 @@ try:
     
     # option_2 = st.selectbox("Selecteer een gebied",gdf_areas['Wijk'].unique(),
     #                         label_visibility="visible",help="Selecteer een gebied om het bemonsteringsproces te beoordelen. Door op de punten te klikken, kunt u gedetailleerde informatie over de individuele enquête ophalen.")
-        df_dagverslag_option_2 = df_dagverslag#[df_dagverslag['gebied_id']==option_2]
+        df_dagverslag_option_2 = df_dagverslag[df_dagverslag['doel']==opdracht]
         df_dagverslag_option_2['datum'] = pd.to_datetime(df_dagverslag_option_2['datum'])
         df_dagverslag_option_2['year'] = df_dagverslag_option_2['datum'].dt.year
         
@@ -226,7 +226,7 @@ try:
                      alt.Tooltip("opmerking:N",title ="Opmerking"),
                     ],
         ).properties(
-            height=300,
+            height=400,
             title=alt.Title(
                 text="",
                 subtitle="",
