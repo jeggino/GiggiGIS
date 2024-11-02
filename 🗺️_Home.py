512ac9c09e095bfb2592accfc77627513d3c0fbd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-02 09:11:39 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -79,7 +79,7 @@ legend_template = """
     <li><span style='background: green; opacity: 0.75;'></span>Gewone dwergvleermuis</li>
     <li><span style='background: yellow; opacity: 0.75;'></span>Laatvlieger</li>
     <li><span style='background: orange; opacity: 0.75;'></span>Watervleermuis</li>
-    <li></li>
+    <li> </li>
     <li><strong>Funtie</strong></li>
     <li><span class="fa fa-circle" style="color:grey" opacity: 0.75;'></span>Geen</li>
     <li><span class="fa fa-star" style="color:grey" opacity: 0.75;'></span>Zommerverblijf</li>
