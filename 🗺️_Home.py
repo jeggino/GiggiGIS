bb8ea4dd78dac6622e1726ad13be9e6da167db8a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-02 08:18:51 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -72,7 +72,7 @@ legend_template = """
 {% macro html(this, kwargs) %}
 <div id='maplegend' class='maplegend' 
     style='position: absolute; z-index: 9999; background-color: rgba(255, 255, 255, 0.8);
-     border-radius: 6px; padding: 10px; font-size: 10.5px; right: 20px; bottom: 20px;'>     
+     border-radius: 6px; padding: 10px; font-size: 10.5px; right: 20px; bottom: -20px;'>     
 <div class='legend-scale'>
   <ul class='legend-labels'>
     <li><span style='background: green; opacity: 0.75;'></span>Wind speed <= 55.21</li>
