4a6985200dc68785225d800a4e84e546845764a5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-19 09:48:55 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -54,13 +54,59 @@ reduce_header_height_style = """
 
 st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
-from functions import *
 
 #---DATASET---
 ttl = 0
 ttl_references = '10m'
 conn = st.connection("gsheets", type=GSheetsConnection)
 df_point = conn.read(ttl=ttl,worksheet="ratten-terschelling")
+
+#---FUNCTIONS---
+def popup_html(row,df_2):
+    
+    i = row
+
+    datum=df_2['datum'].iloc[i] 
+    datum_2=df_2['datum_2'].iloc[i] 
+    functie=df_2['functie'].iloc[i]
+    opmerking=df_2['opmerking'].iloc[i]
+    waarnemer=df_2['waarnemer'].iloc[i] 
+       
+
+    left_col_color = "#19a7bd"
+    right_col_color = "#f2f0d3"
+    
+    html = """<!DOCTYPE html>
+    <html>
+    <table style="height: 126px; width: 300;">
+    <tbody>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum verwijderd</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum_2) + """
+    </tr>
+    <tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
+    </tr>
+
+    </tbody>
+    </table>
+    </html>
+    """
+    return html
 
 # --- APP ---
 IMAGE = "icon/menu.jpg"
