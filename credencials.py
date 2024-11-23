0fcc7fb08acfb6e411638e9819c5a57c05f05b21
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-23 01:52:05 +0100

    Update credencials.py

--- credencials.py
+++ credencials.py
@@ -11,7 +11,8 @@ OUTPUT_height = 550
 
 # --- DIMENSIONS ---
 ICON_SIZE = (20,20)
-ICON_SIZE_trap = (100,75)
+ICON_SIZE_trap_empty = (50,35)
+ICON_SIZE_trap_noempty = (75,45)
 ICON_SIZE_rat_maybe = (255,150)
 
 # --- OPTIONS ---
