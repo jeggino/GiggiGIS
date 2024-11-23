7c4e200438b0ef5a32448d7ee1ac4dcb0e59f07c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-23 01:29:14 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -64,7 +64,7 @@ df_point = conn.read(ttl=ttl,worksheet="ratten-terschelling")
 
 # --- APP ---
 
-st.logo(LOGO,  link="https://www.elskenecologie.nl/#:~:text=Elsken%20Ecologie%20is%20het%20onafhankelijke%20ecologisch%20advies-%20en", icon_image=LOGO)
+st.logo(LOGO,  size="medium", link="https://www.elskenecologie.nl/#:~:text=Elsken%20Ecologie%20is%20het%20onafhankelijke%20ecologisch%20advies-%20en", icon_image=LOGO)
 
 # try:
     
