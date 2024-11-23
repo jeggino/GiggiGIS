7d196fc4cd8b66920e5d4b375efa3a4c257b1e4e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-23 01:49:36 +0100

    Update 🗺️_Home.py

--- 🗺️_Home.py
+++ 🗺️_Home.py
@@ -127,12 +127,11 @@ Fullscreen(position="topright").add_to(map)
 
 for i in range(len(df_2)):
 
-    if df_2.iloc[i]['functie'] in ["Waarneming rat doorgegeven, geen actie op ondernomen",
-                                   "Rat geschoten",'vangkooi in veld','vangkooi verwijderd, rat gevangen']:
+    if df_2.iloc[i]['functie'] in ["Waarneming rat doorgegeven, geen actie op ondernomen","Rat geschoten"]:
         ICON_SIZE_2 = ICON_SIZE_rat_maybe
 
-    elif df_2.iloc[i]['functie'] == "Rat geschoten":
-        ICON_SIZE_2 = ICON_SIZE_rat_maybe
+    elif df_2.iloc[i]['functie'] in ['vangkooi in veld','vangkooi in veld, rat gevangen']:
+        ICON_SIZE_2 = ICON_SIZE_trap
 
     else:
         ICON_SIZE_2 = ICON_SIZE
