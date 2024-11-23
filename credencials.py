6265e26f73456a0b84318b1cf41ab5aaa7299ad0
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-23 01:17:16 +0100

    Update credencials.py

--- credencials.py
+++ credencials.py
@@ -12,7 +12,7 @@ OUTPUT_height = 550
 # --- DIMENSIONS ---
 ICON_SIZE = (20,20)
 ICON_SIZE_huismus = (28,28)
-ICON_SIZE_rat_maybe = (255,150)
+ICON_SIZE_rat_maybe = (25,15)
 
 # --- OPTIONS ---
 soortgroup =["📷 Camera", "💉 Rat val", '🪤 Vangkooi','🔫 Rat geschoten']
