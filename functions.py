973ca31ae3dc598ddc1e48f92d274540b0ea6949
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-26 09:19:20 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ def popup_html(row,df_2):
     functie=df_2['functie'].iloc[i]
     opmerking=df_2['opmerking'].iloc[i]
     waarnemer=df_2['waarnemer'].iloc[i] 
-    aantal=int(df_2['aantal'].iloc[i])
+    aantal=df_2['aantal'].iloc[i]
        
 
     left_col_color = "#19a7bd"
