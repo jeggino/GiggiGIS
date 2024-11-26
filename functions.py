e705b1149ebb5ffed7b4ef89407f170221bc63d2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-26 11:45:19 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -52,6 +52,34 @@ def popup_html(row,df_2):
         </table>
         </html>
         """
+
+    elif functie == 'Waarneming rat doorgegeven, geen actie op ondernomen':
+        html = """<!DOCTYPE html>
+        <html>
+        <table style="height: 126px; width: 300;">
+        <tbody>
+        <tr>
+        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
+        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
+        </tr>
+        <tr>
+        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
+        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
+        </tr>  
+        <tr>
+        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
+        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
+        </tr>
+        <tr>
+        <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
+        <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
+        </tr>
+    
+        </tbody>
+        </table>
+        </html>
+        """
+        
     else:    
         html = """<!DOCTYPE html>
         <html>
